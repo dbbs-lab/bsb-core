2ddc4974e921c880e03b4fc14ef8decdfd18bba6
Robin De Schepper <robin.deschepper93@gmail.com>
2019-07-16 11:32:38 +0200

    added geometry stubs

--- scaffold/geometries.py
+++ scaffold/geometries.py
@@ -6,6 +6,17 @@ class Geometry(abc.ABC):
     def __init__(self):
         pass
 
-
 class GranuleCellGeometry(Geometry):
+    pass
+
+class PurkinjeCellGeometry(Geometry):
+    pass
+
+class GolgiCellGeometry(Geometry):
+    pass
+
+class RadialGeometry(Geometry):
+    pass
+
+class NoGeometry(Geometry):
     pass
