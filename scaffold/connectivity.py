760dcb51804bf4556986c01c219dcee4dd80aace
Robin De Schepper <robin.deschepper93@gmail.com>
2019-07-30 15:15:29 +0200

    imported glom_goc connectome

--- scaffold/connectivity.py
+++ scaffold/connectivity.py
@@ -36,7 +36,7 @@ class TouchConnect(ConnectionStrategy):
 	def connect(self):
 		pass
 
-class ConnectomeGlomGranule(TouchingConvergenceDivergence):
+class ConnectomeGlomerulusGranule(TouchingConvergenceDivergence):
 	'''
 		Legacy implementation for the connections between glomeruli and granule cells.
 	'''
@@ -89,3 +89,42 @@ class ConnectomeGlomGranule(TouchingConvergenceDivergence):
 
 		# Execute legacy code and return the connection matrix it returns.
 		return connectome_glom_grc(first_glomerulus, glomeruli, granules, dend_len, n_conn_glom)
+
+class ConnectomeGlomerulusGolgi(TouchingConvergenceDivergence):
+	'''
+		Legacy implementation for the connections between glomeruli and Golgi cells.
+	'''
+	def validate(self):
+		pass
+
+	def connect(self):
+		# Gather information for the legacy code block below.
+		glomerulus_celltype = self.from_celltype
+		golgi_celltype = self.to_celltype
+		glomeruli = self.scaffold.cells_by_type[glomerulus_celltype.name]
+		golgis = self.scaffold.cells_by_type[golgi_celltype.name]
+		first_glomerulus = int(glomeruli[0,0])
+		r_goc_vol = golgi_celltype.geometry.dendrite_radius
+
+		def connectome_glom_goc(first_glomerulus, glomeruli, golgicells, r_goc_vol):
+			glom_bd = np.zeros((0,3))
+
+			# for all Golgi cells: calculate which glomeruli fall into the volume of GoC basolateral dendrites, then choose 40 of them for the connection and delete them from successive computations, since 1 axon is connected to 1 GoC
+			for i in golgicells:
+
+				volume_matrix = (((glomeruli[:,2]-i[2])**2)+((glomeruli[:,3]-i[3])**2)+((glomeruli[:,4]-i[4])**2)-(r_goc_vol**2)).__le__(0) & (glomeruli[:,3]).__le__(i[3])
+				good_gloms = np.where(volume_matrix==True)[0]	# finds indexes of granules that can potentially be connected
+				connected_gloms = good_gloms + first_glomerulus
+
+				# construction of the output matrix: the first column has the index of the connected glomerulus, while the second column has the Golgi cell index
+				matrix = np.zeros((len(good_gloms), 3))
+				matrix[:,1] = i[0]
+				matrix[:,0] = connected_gloms
+				matrix[:,2] = np.sqrt((glomeruli[good_gloms,2]-i[2])**2 + (glomeruli[good_gloms,3]-i[3])**2 + (glomeruli[good_gloms,4]-i[4])**2)
+				glom_bd = np.vstack((glom_bd, matrix))
+
+			glom_bd = glom_bd[1:-1,:]
+
+			return glom_bd
+
+		return connectome_glom_goc(first_glomerulus, glomeruli, golgis, r_goc_vol)
--- scaffold/geometries.py
+++ scaffold/geometries.py
@@ -4,8 +4,8 @@ from .helpers import ConfigurableClass
 
 class Geometry(ConfigurableClass):
 
-    def __init__(self):
-        pass
+	def __init__(self):
+		pass
 
 class GranuleCellGeometry(Geometry):
 	casts = {
@@ -17,26 +17,26 @@ class GranuleCellGeometry(Geometry):
 		pass
 
 class PurkinjeCellGeometry(Geometry):
-    def validate(self):
-        pass
+	def validate(self):
+		pass
 
 class GolgiCellGeometry(Geometry):
-    casts = {
-        'dendrite_radius': float,
-        'axon_x': float,
-        'axon_y': float,
-        'axon_z': float,
-    }
+	casts = {
+		'dendrite_radius': float,
+		'axon_x': float,
+		'axon_y': float,
+		'axon_z': float,
+	}
 
 	required = ['dendrite_radius']
 
-    def validate(self):
-        pass
+	def validate(self):
+		pass
 
 class RadialGeometry(Geometry):
-    def validate(self):
-        pass
+	def validate(self):
+		pass
 
 class NoGeometry(Geometry):
-    def validate(self):
-        pass
+	def validate(self):
+		pass
