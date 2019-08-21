eb21f03fa1c786f106dac688fd18df0c5b8776c9
Robin De Schepper <robin.deschepper93@gmail.com>
2019-08-21 16:14:11 +0200

    imported connectome_aa_pc. dropped `aa_pc` parameter which is instead initialized inside function. dropped distance column from connectome matrix.

--- scaffold/connectivity.py
+++ scaffold/connectivity.py
@@ -224,7 +224,7 @@ class ConnectomeGranuleGolgi(ConnectionStrategy):
 		golgis = self.scaffold.cells_by_type[golgi_celltype.name]
 		first_granule = int(granules[0, 0])
 		r_goc_vol = golgi_celltype.geometry.dendrite_radius
-		oob = self.scaffold.configuration.X * 1000.
+		oob = self.scaffold.configuration.X * 1000. # Any arbitrarily large value outside of simulation volume
 		n_connAA = self.aa_convergence
 		n_conn_pf = self.pf_convergence
 		tot_conn = n_connAA + n_conn_pf
@@ -300,7 +300,7 @@ class ConnectomeGranuleGolgi(ConnectionStrategy):
 
 class ConnectomeGolgiGranule(ConnectionStrategy):
 	'''
-		Legacy implementation for the connections between Golgi cells and glomeruli.
+		Legacy implementation for the connections between Golgi cells and granule cells.
 	'''
 
 	def validate(self):
@@ -341,3 +341,53 @@ class ConnectomeGolgiGranule(ConnectionStrategy):
 
 		result = connectome_goc_grc(glom_grc, goc_glom)
 		self.scaffold.connect_cells(self, result)
+
+class ConnectomeAscAxonPurkinje(ConnectionStrategy):
+	'''
+		Legacy implementation for the connections between ascending axons and purkinje cells.
+	'''
+
+	def validate(self):
+		pass
+
+	def connect(self):
+		# Gather information for the legacy code block below.
+		granule_celltype = self.from_celltype
+		purkinje_celltype = self.to_celltype
+		granules = self.scaffold.cells_by_type[granule_celltype.name]
+		purkinjes = self.scaffold.cells_by_type[purkinje_celltype.name]
+		first_granule = int(granules[0, 0])
+		OoB_value = self.scaffold.configuration.X * 1000. # Any arbitrarily large value outside of simulation volume
+		purkinje_extension_x = purkinje_celltype.placement.extension_x
+		purkinje_extension_z = purkinje_celltype.placement.extension_z
+
+		def connectome_aa_pc(first_granule, granules, purkinjes, x_pc, z_pc, OoB_value):
+			aa_pc = np.zeros((0, 2))
+			new_granules = np.copy(granules)
+
+			# for all Purkinje cells: calculate and choose which granules fall into the area of PC dendritic tree, then delete them from successive computations, since 1 ascending axon is connected to only 1 PC
+			for i in purkinjes:
+
+				# CONTROLLARE X E Z
+				bool_matrix = np.zeros((new_granules.shape[0], 2))	# matrix initialization
+				# ascending axon falls into the z range of values?
+				bool_matrix[:,0] = (new_granules[:,4]).__ge__(i[4]-z_pc/2.) & (new_granules[:,4]).__le__(i[4]+z_pc/2.)
+				# ascending axon falls into the x range of values?
+				bool_matrix[:,1] = (new_granules[:,2]).__ge__(i[2]-x_pc/2.) & (new_granules[:,2]).__le__(i[2]+x_pc/2.)
+
+				good_aa = np.where(np.sum(bool_matrix, axis=1)==2)[0]	# finds indexes of ascending axons that, on the selected axis, have the correct sum value
+
+				# construction of the output matrix: the first column has the GrC id, while the second column has the PC id
+				matrix = np.zeros((len(good_aa), 2))
+				matrix[:,1] = i[0]
+				matrix[:,0] = good_aa + first_granule
+				aa_pc = np.vstack((aa_pc, matrix))
+
+				new_granules[good_aa,:] = OoB_value	# update the granules matrix used for computation by deleting the coordinates of connected ones
+
+
+			return aa_pc
+
+		result = connectome_aa_pc(first_granule, granules, purkinjes, purkinje_extension_x, purkinje_extension_z, OoB_value)
+		print("{} purkinjes, {} connectome shape".format(purkinjes.shape[0], result.shape))
+		self.scaffold.connect_cells(self, result, "AscAxonPurkinje")
