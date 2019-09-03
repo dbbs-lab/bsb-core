936fecf9afa036d9291936d27d89ae2308858a8b
Robin De Schepper <robin.deschepper93@gmail.com>
2019-09-03 16:11:46 +0200

    small changes to setup.py

--- setup.py
+++ setup.py
@@ -2,19 +2,19 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
-     name='scaffold',
-     version='2.0',
+     name='dbbs-scaffold',
+     version='2.0.1',
      scripts=['scaffold.sh', 'scaffold.bat'] ,
-     author="Elisa, Stefano, Claudia, Robin",
-     author_email="scaffold@unipv.com",
+     author="Elisa Marenzi, Stefano Casali, Claudia Casselato, Robin De Schepper",
+     author_email="robingilbert.deschepper@unipv.it",
      description="A morphologically detailed scaffolding package for the scientific modelling of the cerebellum.",
      long_description=long_description,
-   long_description_content_type="text/markdown",
-     url="https://github.com/Helveg/cerebellum-scaffold",
+     long_description_content_type="text/markdown",
+     url="https://github.com/dbbs-lab/scaffold",
+     license='GPLv3',
      packages=setuptools.find_packages(),
      classifiers=[
          "Programming Language :: Python :: 3",
-         "License :: OSI Approved :: MIT License",
          "Operating System :: OS Independent",
      ],
  )
