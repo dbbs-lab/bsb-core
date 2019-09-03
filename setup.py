1e7997242c2f8330c0230132a54204ca75bf23a9
Robin De Schepper <robin.deschepper93@gmail.com>
2019-09-03 19:21:47 +0200

    updated setup.py and moved scripts & ini files to be deployed as pkgdata

--- setup.py
+++ setup.py
@@ -3,11 +3,15 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
      name='dbbs-scaffold',
-     version='2.0.1',
-     scripts=['scaffold.sh', 'scaffold.bat'] ,
+     version='2.0.2',
      author="Elisa Marenzi, Stefano Casali, Claudia Casselato, Robin De Schepper",
      author_email="robingilbert.deschepper@unipv.it",
      description="A morphologically detailed scaffolding package for the scientific modelling of the cerebellum.",
+     scripts=['bin/scaffold', 'bin/scaffold.bat'] ,
+     include_package_data=True,
+     data_files=[
+        ('configurations', ['scaffold/configurations/mouse_cerebellum.ini'])
+     ],
      long_description=long_description,
      long_description_content_type="text/markdown",
      url="https://github.com/dbbs-lab/scaffold",
