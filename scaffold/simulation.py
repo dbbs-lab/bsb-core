ffd3f64e76d9552f4aa613a2e26c4c54688843e7
Alice Geminiani <alice.geminiani@polimi.it>
2019-09-10 11:37:59 +0200

    connect_neuron function
    Added connect_neuron function - to verify with simulation configuration file

--- scaffold/simulation.py
+++ scaffold/simulation.py
@@ -26,7 +26,7 @@ class NestAdapter(SimulatorAdapter):
         scaffold = self.scaffold
         configuration = self.scaffold.configuration
         self.create_neurons(simulation_config,configuration.cell_types)
-        self.connect_neurons()
+        self.connect_neurons(simulation_config, configuration.connection_types)
         self.stimulate_neurons()
         return nest
 
@@ -47,8 +47,16 @@ class NestAdapter(SimulatorAdapter):
             nest.SetDefaults(name, cell_type.simulation.nest.models[nest_model_name])
             # nest.Create(neuron_type.name,neuron_type.placement.number)
 
-    def connect_neurons(self):
-        pass
+    def connect_neurons(self, simulation_config, connection_types):
+        default_model = self.scaffold.simulation_config.synapse_model       # default model will be static_synapse
+        for connection_type in connection_types.values():
+            pre = connection_type.matrix[:,0]          # The connectivity matrix can be in the configuration file? Maybe better in the simulation configuration file?
+            post = connection_type.matrix[:,1]
+            syn_spec = connection_type.conn_parameters  # Dictionary with delay and weight
+            conn_dict = {'rule': 'one_to_one'}
+            nest.Connect(pre,post, conn_dict, syn_spec)
+        
+        
 
     def stimulate_neurons(self):
         pass
