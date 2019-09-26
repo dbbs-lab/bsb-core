f223897aa49ff298413667631841ed2735b376a8
Robin De Schepper <robin.deschepper93@gmail.com>
2019-09-26 18:46:50 +0200

    fixed missing self in constructor

--- scaffold/voxels.py
+++ scaffold/voxels.py
@@ -1,5 +1,5 @@
 class VoxelCloud:
-    def __init__(grid_size, positions, map):
+    def __init__(self, grid_size, positions, map):
         self.grid_size = grid_size
         self.positions = positions
         self.map = map
