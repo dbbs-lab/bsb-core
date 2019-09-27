13c93d191ef8ce4ebde8d281a7c0f181eb853c71
Robin De Schepper <robin.deschepper93@gmail.com>
2019-09-27 14:57:00 +0200

    self.load() contextmanager only closes the handle if it was the one that opened it. refactored type_maps

--- scaffold/output.py
+++ scaffold/output.py
@@ -10,12 +10,15 @@ class ResourceHandler:
 
     @contextmanager
     def load(self):
+        already_open = False
         if self.handle is None:
             self.handle = self.get_handle()
+            already_open = True
         try:
             yield self.handle
         finally:
-            self.release_handle(self.handle)
+            if not already_open:
+                self.release_handle(self.handle)
 
     @abstractmethod
     def get_handle(self):
@@ -129,8 +132,9 @@ class HDF5Formatter(OutputFormatter):
         position_dataset = cells_group.create_dataset('positions', data=self.scaffold.cells)
         cell_type_names = self.scaffold.configuration.cell_type_map
         cells_group.attrs['types'] = cell_type_names
+        type_maps_group = cells_group.create_group('type_maps')
         for type in self.scaffold.configuration.cell_types.keys():
-            position_dataset.attrs[type + '_map'] = np.where(self.scaffold.cells[:,1] == cell_type_names.index(type))[0]
+            type_maps_group.create_dataset(type + '_map', data=np.where(self.scaffold.cells[:,1] == cell_type_names.index(type))[0])
 
     def store_cell_connections(self, cells_group):
         connections_group = cells_group.create_group('connections')
@@ -182,4 +186,9 @@ class HDF5Formatter(OutputFormatter):
             raise Exception("Attempting to load cell type '{}' that isn't defined in the storage.".format(name))
         # Slice out the cells of this type based on the map in the position dataset attributes.
         with self.load() as resource:
-            return resource['/cells/positions'][resource['/cells/positions'].attrs[name + '_map']][()]
+            type_map = self.get_type_map(name)
+            return resource['/cells/positions'][type_map][()]
+
+    def get_type_map(self, type):
+        with self.load() as resource:
+            return self.handle['/cells/type_maps/{}_map'.format(type)][()]
