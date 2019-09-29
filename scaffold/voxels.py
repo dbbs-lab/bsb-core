13d475c573ccbcdeaa9fc12e26e2473948a3c5bb
Robin De Schepper <robin.deschepper93@gmail.com>
2019-09-29 14:39:35 +0200

    MUSICAL CHAIRS!!!!!!!!!!!!!

--- scaffold/voxels.py
+++ scaffold/voxels.py
@@ -4,6 +4,8 @@ from mpl_toolkits import mplot3d
 import numpy as np
 from .plotting import plot_voxelize_results
 from scipy import ndimage
+from random import choice as random_element
+from time import sleep
 
 class VoxelCloud:
     def __init__(self, bounds, voxels, grid_size, map, occupancies=None):
@@ -36,7 +38,7 @@ class VoxelCloud:
     def create(morphology, N):
         hit_detector, box_data = morphology_detector_factory(morphology)
         bounds, voxels, length, error = voxelize(N, box_data, hit_detector)
-        plot_voxelize_results(bounds, voxels, length)
+        # plot_voxelize_results(bounds, voxels, length)
         voxel_map = morphology.get_compartment_map(m_grid(bounds, length), voxels, length)
         if error == 0:
             return VoxelCloud(bounds, voxels, length, voxel_map)
@@ -149,3 +151,232 @@ def center_of_mass(points, weights = None):
     else:
         cog = [np.sum(points[dim, :] * weights) for dim in range(points.shape[0])] / np.sum(weights)
     return cog
+
+def set_attraction(attractor, voxels):
+    attraction_voxels = np.indices(voxels.shape)[:, voxels].T
+    attraction_map = np.zeros(voxels.shape)
+    dist = np.sqrt(np.sum((attraction_voxels - attractor + np.ones(len(attractor)) * 0.5)**2, axis=1))
+    distance_sorting = dist.argsort()[::-1]
+    attraction = 1
+    first_voxel = distance_sorting[0]
+    attraction_map[attraction_voxels[first_voxel,0],attraction_voxels[first_voxel,1],attraction_voxels[first_voxel,2]] = 1
+    last_distance = dist[first_voxel]
+    for v in distance_sorting[1:]:
+        distance = dist[v]
+        attraction += int(distance < last_distance)
+        attraction_map[attraction_voxels[v,0],attraction_voxels[v,1],attraction_voxels[v,2]] = attraction
+        last_distance = distance
+    return attraction_map
+
+class AttractionGame:
+    def __init__(self, attractor, field):
+        self.players = []
+        self.eliminated_players = []
+        self.attractor = attractor
+        self.field = field
+        self.active_players = 0
+        self.occupied = {}
+        self.turn = 0
+        self.artists = []
+
+    def clear(self):
+        self.players = []
+        self.eliminated_players = []
+        self.turn = 0
+        self.occupied = {}
+        for artist in self.artists:
+            artist.remove()
+        self.artists = []
+
+    def occupy(self, player, new_position):
+        if new_position in self.occupied:
+            raise Exception("Position already occupied")
+        self.unoccupy(player.position)
+        self.occupied[new_position] = True
+        player.position = new_position
+
+    def unoccupy(self, position):
+        if position in self.occupied:
+            del self.occupied[position]
+
+    def add_player(self, payload, position):
+        if position in self.occupied:
+            raise Exception("Position already occupied")
+        player = AttractionPlayer(self, payload, position)
+        self.players.append(player)
+        self.occupied[position] = True
+        self.active_players += 1
+        return player
+
+    def eliminate_player(self, player):
+        self.players.remove(player)
+        self.eliminated_players.append(player)
+        self.active_players -= 1
+
+    def is_unoccupied(self, position):
+        return not tuple(position) in self.occupied
+
+    def is_out_of_bounds(self, position):
+        p = np.array(position)
+        return np.sum((p < 0) | (p >= self.field.shape)) > 0
+
+    def get_attraction(self, position):
+        p = tuple(position)
+        if self.is_out_of_bounds(p):
+            return 0
+        return self.field[p]
+
+    def get_attractions(self, candidates):
+        return [self.get_attraction(p) for p in candidates]
+
+    def play(self):
+        fig = plt.figure()
+        ax = fig.gca(projection='3d')
+        ax.set(xlabel='x', ylabel='z', zlabel='y')
+        ax.voxels(np.swapaxes(self.field > 0, 1, 2), facecolors=(0.,0.,0.,0.0), edgecolor='k', linewidth=.25)
+        com = self.attractor
+        ax.scatter([com[0]],[com[2]],[com[1]], s=30, c=[(1., 0., 0.)])
+        plt.interactive(True)
+        plt.show()
+        self.turn = 0
+        self.set_plot_limits(ax)
+        self.plot_turn(fig, ax)
+        # sleep(1)
+        while self.active_players > 0:
+            self.turn += 1
+            # print('####### TURN ', self.turn)
+            closest_player_first = self.get_closest_players()
+            for p in closest_player_first:
+                best = p.move()
+            self.plot_turn(fig, ax)
+            sleep(0.5)
+        plt.show(block=True)
+
+
+    def get_closest_players(self):
+        positions = list(map(lambda p: p.position, self.players))
+        distances = self.get_attractor_distances(positions)
+        return np.array(self.players)[np.argsort(distances)].tolist()
+
+    def get_attractor_distances(self, candidates):
+        dists = get_distances(candidates, self.attractor)
+        return dists
+
+    def get_plot_voxels(self):
+        pass
+
+    def plot_turn(self, fig, ax):
+        for artist in self.artists:
+            artist.remove()
+        self.artists = []
+        for p in self.players:
+            # Draw player
+            self.artists.append(ax.scatter([p.position[0] + 0.5],[p.position[2] + 0.5],[p.position[1] + 0.5], c=[p.color]))
+            self.artists.append(ax.plot(xs=[p.last_move[0] + 0.5, p.position[0] + 0.5],ys=[p.last_move[2] + 0.5, p.position[2] + 0.5], zs=[p.last_move[1] + 0.5, p.position[1] + 0.5], c=(0., 1., 0.), linewidth=.25)[0])
+        for p in self.eliminated_players:
+            self.artists.append(ax.scatter([p.position[0] + 0.5],[p.position[2] + 0.5],[p.position[1] + 0.5], c=[(0.5, 0.5, 0.5, 0.5)]))
+        fig.canvas.draw()
+        fig.canvas.flush_events()
+
+    def set_plot_limits(self, ax):
+        positions = list(map(lambda p: p.position, self.players))
+        if len(positions) == 0:
+            min_ax = [0., 0., 0.]
+            max_ax = np.array(self.field.shape) + 1
+        else:
+            np_pos = np.array(positions)
+            min_ax = np.array([min(np.min(np_pos[:,0]),0), min(np.min(np_pos[:,1]),0), min(np.min(np_pos[:,2]),0)]) + 1
+            max_ax = np.array([max(np.max(np_pos[:,0]),self.field.shape[0]), max(np.max(np_pos[:,1]),self.field.shape[1]), max(np.max(np_pos[:,2]),self.field.shape[2])]) + 1
+        ax.set(xlim=(min_ax[0], max_ax[0]), ylim=(min_ax[2], max_ax[2]), zlim=(min_ax[1], max_ax[1]))
+
+
+
+
+class AttractionPlayer:
+    def __init__(self, game, payload, position):
+        pos = tuple(position)
+        self.game = game
+        self.id = len(game.players)
+        self.payload = payload
+        self.position = pos
+        self.attraction = game.get_attraction(pos)
+        self.eliminated = False
+        self.moves = set([pos])
+        self.last_move = pos
+        self.color = np.random.rand(3)
+
+    def eliminate(self):
+        self.eliminated = True
+        self.game.eliminate_player(self)
+
+    def neighbors(self):
+        x = self.position[0] - 1
+        y = self.position[1] - 1
+        z = self.position[2] - 1
+        neighbors = np.indices((3,3,3))
+        neighbors[0,:,:,:] += x
+        neighbors[1,:,:,:] += y
+        neighbors[2,:,:,:] += z
+        return neighbors
+
+    def filter_unoccupied(self, candidates):
+        return list(filter(lambda c: self.game.is_unoccupied(c), candidates))
+
+    def filter_attractive(self, candidates):
+        if self.attraction == 0: # You're losing the game buddy, you can't be picky
+            return candidates
+        return list(filter(lambda c: self.game.get_attraction(c) > self.attraction, candidates))
+
+    def filter_unvisited(self, candidates):
+        return list(set(candidates) - self.moves)
+
+    def candidates_from_indices(self, indices):
+        all_mask = np.ones(indices.shape[1:], dtype=bool)
+        coords = indices[:, all_mask]
+        candidates = [(coords[0, i], coords[1, i], coords[2, i]) for i in range(coords.shape[1])]
+        # print('cand:', candidates)
+        return candidates
+
+    def get_available_moves(self):
+        candidates = self.candidates_from_indices(self.neighbors())
+        unvisited_candidates = self.filter_unvisited(candidates)
+        open_candidates = self.filter_unoccupied(unvisited_candidates)
+        attractive_candidates = self.filter_attractive(open_candidates)
+        return attractive_candidates
+
+    def get_best_moves(self):
+        available_moves = self.get_available_moves()
+        if len(available_moves) == 0:
+            return None
+        attractions = self.game.get_attractions(available_moves)
+        return find_best_candidates(available_moves, attractions)
+
+    def get_move(self):
+        best_moves = self.get_best_moves()
+        if best_moves is None:
+            return None
+        distances = self.game.get_attractor_distances([(p[0] + 0.5, p[1] + 0.5, p[2] + 0.5) for p in best_moves])
+        closest = find_best_candidates(best_moves, distances, metric=np.min)
+        closest_to_me = find_best_candidates(closest, get_distances(closest, self.position), metric=np.min)
+        return random_element(closest_to_me)
+
+    def move(self):
+        m = self.get_move()
+        if m is None:
+            # print('player {}: eliminated at {} with score {}'.format(self.id, self.position, self.game.turn, self.game.get_attraction(self.position)))
+            self.eliminate()
+            return
+        # print('player {}: moved from {} to {}'.format(self.id, self.position, m))
+        self.last_move = self.position
+        self.moves.add(m)
+        self.game.occupy(self, m)
+        self.attraction = self.game.get_attraction(m)
+        # print('last move is now:',self.last_move, self.position)
+
+def find_best_candidates(candidates, results, metric=np.max):
+    best_result = metric(results)
+    highest_scoring = [candidates[i] if results[i] == best_result else False for i in range(len(results))]
+    return list(filter(bool, highest_scoring))
+
+def get_distances(candidates, point):
+    return [np.sqrt(np.sum((np.array(c) - point) ** 2)) for c in candidates]
