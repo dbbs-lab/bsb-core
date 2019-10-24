f8e53ad3aaee95efd0372ea9a503c7a0f385db5d
Robin De Schepper <robin.deschepper93@gmail.com>
2019-10-24 19:24:01 +0200

    added double neuron type stub test

--- tests/test_plasticity.py
+++ tests/test_plasticity.py
@@ -5,7 +5,7 @@ from scaffold.config import JSONConfig
 from scaffold.simulators.nest import NestCell
 from scaffold.models import Layer, CellType
 
-class TestSingleNeuronSetup(unittest.TestCase):
+class TestSingleNeuronTypeSetup(unittest.TestCase):
 
     def setUp(self):
         config = JSONConfig(file="test_single_neuron.json")
@@ -17,4 +17,20 @@ class TestSingleNeuronSetup(unittest.TestCase):
     def test_single_neuron(self):
         self.scaffold.run_simulation("test_single_neuron")
         test_cell_model = self.nest_adapter.cell_models["test_cell"]
-        self.assertEqual(test_cell_model.identifiers, [1])
+        self.assertEqual(test_cell_model.identifiers, [1, 2, 3])
+
+
+class TestDoubleNeuronTypeSetup(unittest.TestCase):
+
+    def setUp(self):
+        config = JSONConfig(file="test_double_neuron.json")
+        self.scaffold = Scaffold(config)
+        self.scaffold.compile_network()
+        self.nest_adapter = self.scaffold.configuration.simulations['test_double_neuron']
+
+    def test_double_neuron(self):
+        self.scaffold.run_simulation("test_double_neuron")
+        test_cell_model = self.nest_adapter.cell_models["from_cell"]
+        self.assertEqual(test_cell_model.identifiers, [1, 2, 3, 4])
+        test_cell_model = self.nest_adapter.cell_models["to_cell"]
+        self.assertEqual(test_cell_model.identifiers, [5, 6, 7, 8])
