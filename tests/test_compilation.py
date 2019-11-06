7dca206fe16b4b2adfe85d60859765a40d010890
Claudia Casellato <claudia.casellato@unipv.it>
2019-11-06 14:54:54 +0100

    added test on PC placement

--- tests/test_compilation.py
+++ tests/test_compilation.py
@@ -26,3 +26,37 @@ class TestSingleTypeCompilation(unittest.TestCase):
     def test_hdf5_cells(self):
         pass
         # TODO: Implement a check that the hdf5 file contains the right datasets under 'cells'
+
+class TestPlacement(unittest.TestCase):
+    '''
+    Check if the placement of all cell types is correct
+    '''
+
+    @classmethod
+    def setUpClass(self):
+        super(TestPlacement, self).setUpClass()
+        config = JSONConfig(file="../scaffold/configurations/mouse_cerebellum.json")
+        self.scaffold = Scaffold(config)
+
+    def test_purkinje(self):
+        import scipy.spatial.distance as dist
+        config = self.scaffold.configuration
+        layer = config.layers['purkinje_layer']
+        pc = config.cell_types['purkinje_cell']
+        self.scaffold.reset_network_cache()
+        pc.placement.place(pc)
+        pcCount = self.scaffold.cells_by_type['purkinje_cell'].shape[0]
+        density = pcCount / layer.width / layer.depth
+        pc_pos = self.scaffold.cells_by_type['purkinje_cell'][:, [2,3,4]]
+        Dist2D = dist.pdist(np.column_stack((pc_pos[:,0],pc_pos[:,2])), 'euclidean')
+        overlapSomata = np.where(Dist2D < 80/100*pc.placement.soma_radius)[0] #
+        Dist1Dsqr=np.zeros(( pc_pos.shape[0],pc_pos.shape[0],2))
+        Dist1Dsqr[:,:,0] = dist.squareform(dist.pdist(pc_pos[:,[0]], 'euclidean'))
+        Dist1Dsqr[:,:,1] = dist.squareform(dist.pdist(pc_pos[:,[2]], 'euclidean'))
+        overlapDend_whichPairs = np.where ( np.logical_and (Dist1Dsqr[:,:,0] <80/100*pc.placement.extension_x, Dist1Dsqr[:,:,1]<80/100*pc.placement.extension_z ) )
+        overlapDend_whichPairs = np.column_stack((overlapDend_whichPairs[0],overlapDend_whichPairs[1]))
+        overlapDend_whichPairs = overlapDend_whichPairs[np.where (overlapDend_whichPairs[:,0]!=overlapDend_whichPairs[:,1])[0],:]
+
+        # Asserts
+        self.assertAlmostEqual(overlapSomata.shape[0],0, delta=pcCount*1/100)
+        self.assertAlmostEqual(overlapDend_whichPairs.shape[0]/2,0, delta=pcCount*4/100)
