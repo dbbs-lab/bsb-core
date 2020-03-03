59108a8d939bf0daa578eb0f2654fde4aa427b1a
Robin De Schepper <robin.deschepper93@gmail.com>
2020-03-03 14:13:34 +0100

    Split placement.py into its own module. (#302)
    * Split placement.py into its own module like the connectivity.
    
    This should make further work for parallel placement easoer
    
    * Imported exceptions
    
    * removed debug prints

---
+++ scaffold/placement/__init__.py
@@ -0,0 +1,11 @@
+from .strategy import (
+    PlacementStrategy,
+    MightBeRelative,
+    MustBeRelative,
+    Layered,
+    Entities,
+)
+from .randomwalk import LayeredRandomWalk
+from .arrays import ParallelArrayPlacement
+from .particle import ParticlePlacement
+from .satellite import Satellite
---
+++ scaffold/placement/particle.py
@@ -0,0 +1,62 @@
+from .strategy import Layered, PlacementStrategy
+from ..particles import ParticleSystem
+from ..exceptions import *
+
+
+class ParticlePlacement(Layered, PlacementStrategy):
+
+    casts = {
+        "prune": bool,
+        "bounded": bool,
+    }
+
+    defaults = {
+        "prune": True,
+        "bounded": False,
+    }
+
+    def place(self):
+        cell_type = self.cell_type
+        layer = self.layer_instance
+        origin = layer.origin.copy()
+        # Shift voxel origin up based on y_restriction.
+        origin[1] = layer.origin[1] + layer.thickness * self.restriction_minimum
+        # Computing voxel thickness based on y_restriction
+        volume = [layer.width, layer.thickness * self.restriction_factor, layer.depth]
+        # Create a list of voxels with the current restricted layer as only voxel.
+        voxels = [[origin, volume]]
+        # Define the particles for the particle system.
+        particles = [
+            {
+                "name": cell_type.name,
+                "voxels": [0],
+                "radius": cell_type.placement.radius,
+                "count": self.get_placement_count(),
+            }
+        ]
+        # Create and fill the particle system.
+        system = ParticleSystem(track_displaced=True, scaffold=self.scaffold)
+        system.fill(voxels, particles)
+        # Raise a warning if no cells could be placed in the volume
+        if len(system.particles) == 0:
+            self.scaffold.warn(
+                "Did not place any {} cell in the {}!".format(cell_type.name, layer.name),
+                PlacementWarning,
+            )
+            return
+
+        # Find the set of colliding particles
+        colliding = system.find_colliding_particles()
+        if len(colliding) > 0:
+            system.solve_collisions()
+            if self.prune:
+                number_pruned = system.prune(at_risk_particles=system.displaced_particles)
+                self.scaffold.report(
+                    "{} {} ({}%) cells pruned.".format(
+                        number_pruned,
+                        cell_type.name,
+                        int((number_pruned / self.get_placement_count()) * 100),
+                    )
+                )
+        particle_positions = system.positions
+        self.scaffold.place_cells(cell_type, layer, particle_positions)
