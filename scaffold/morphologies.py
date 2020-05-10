cb502569c231da0bf791a5b3e0ccb904d1ce9f0c
Robin De Schepper <robin.deschepper93@gmail.com>
2020-05-10 18:54:06 +0200

    Added `from_template` and `to_record` methods to `Compartment`

--- scaffold/morphologies.py
+++ scaffold/morphologies.py
@@ -7,6 +7,11 @@ from .reporting import report
 
 
 class Compartment:
+    # TODO: The init now too tightly couples every use of Compartment to how it's stored
+    # in HDF5 forcing everything to use the record array even when it isn't being loaded
+    # or saved to storage. --> Make a base Compartment class with a regular constructor
+    # and either a `from_record` class method or a derived StoredCompartment class that
+    # uses this constructor.
     def __init__(self, morphology, repo_record):
         """
             Create a compartment from repository data.
@@ -26,6 +31,27 @@ class Compartment:
         self.spherical = np.sqrt((self.start[:] - self.end[:]) ** 2) / 2
 
         self.morphology = morphology
+
+    @classmethod
+    def from_template(cls, template, **kwargs):
+        """
+            Create a compartment that copies over all information of a template compartment
+            and accepts any keyword to overwrite or add attributes.
+        """
+        c = cls(template.morphology, template.to_record())
+        for k, v in kwargs.items():
+            c.__dict__[k] = v
+        return c
+
+    def to_record(self):
+        """
+            Return an array that can be used to store this compartment in an HDF5 dataset,
+            or to construct a new Compartment.
+        """
+        record = [self.id, self.type, *self.start, *self.end, self.radius, self.parent]
+        if hasattr(self, "section_id"):
+            record.append(self.section_id)
+        return record
 
 
 class Morphology(ConfigurableClass):
