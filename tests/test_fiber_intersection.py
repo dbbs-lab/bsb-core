0549756a8dcebe8e41c1338b7a42bdeabadb668e
Alice <alice.geminiani@polimi.it>
2020-05-26 00:20:17 +0200

    Added first fiber_intersection test

---
+++ tests/test_fiber_intersection.py
@@ -0,0 +1,36 @@
+import unittest, os, sys, numpy as np, h5py, importlib
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
+from scaffold.core import Scaffold
+from scaffold.config import JSONConfig
+from scaffold.models import Layer, CellType, ConnectivitySet
+from scaffold.output import MorphologyRepository
+from test_setup import get_test_network
+
+
+def relative_to_tests_folder(path):
+    return os.path.join(os.path.dirname(__file__), path)
+
+
+fiber_transform_config = relative_to_tests_folder("configs/test_fiber_intersection.json")
+morpho_file = relative_to_tests_folder("morphologies_fiber.hdf5")
+
+
+class TestFiberIntersection(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        super(TestFiberIntersection, self).setUpClass()
+        # The scaffold has only the Granular layer (100x100x150) with 20 GrCs and 1 GoC placed, as specified in the
+        # config file, where the densities are reduced to obtain those neuron numbers
+        config = JSONConfig(file=fiber_transform_config)
+        self.scaffold = Scaffold(config)
+        self.scaffold.morphology_repository = MorphologyRepository(morpho_file)
+        self.scaffold.compile_network()
+
+    def test_fiber_connections(self):
+        pre_type = "granule_cell"
+        pre_neu = self.scaffold.get_placement_set(pre_type)
+        conn_type = "parallel_fiber_to_golgi"
+        cs = self.scaffold.get_connectivity_set(conn_type)
+        # Check that no more connections are formed than the number of presynaptic neurons - how could happen otherwise?
+        self.assertTrue(len(cs.connections) <= len(pre_neu.identifiers))
