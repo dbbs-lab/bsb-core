4045e4d1944b6acfdfe464e4c8cd65f7a352b2db
Alice <alice.geminiani@polimi.it>
2020-05-27 17:35:43 +0200

    Added test bending

--- tests/test_fiber_intersection.py
+++ tests/test_fiber_intersection.py
@@ -23,6 +23,16 @@ class TestFiberIntersection(unittest.TestCase):
         # The scaffold has only the Granular layer (100x100x150) with 20 GrCs and 1 GoC placed, as specified in the
         # config file
         config = JSONConfig(file=fiber_transform_config)
+        quivers_field = np.zeros(
+            shape=(3, 4, 4, 6)
+        )  # Each voxel in the volume has vol_res=25um
+        basic_quiver = np.array([0, 0.7, 0.7])
+        quivers_field[0, :] = basic_quiver[0]
+        quivers_field[1, :] = basic_quiver[1]
+        quivers_field[2, :] = basic_quiver[2]
+        config.connection_types[
+            "parallel_fiber_to_golgi_bended"
+        ].transformation.quivers = quivers_field
         self.scaffold = Scaffold(config)
         self.scaffold.morphology_repository = MorphologyRepository(morpho_file)
         self.scaffold.compile_network()
@@ -36,16 +46,18 @@ class TestFiberIntersection(unittest.TestCase):
         # Check that no more connections are formed than the number of presynaptic neurons - how could happen otherwise?
         self.assertTrue(num_conn <= len(pre_neu.identifiers))
 
-        # Check that increasing resolution in FiberIntersection do not change connection
+        # Check that increasing resolution in FiberIntersection does not change connection
         # number if there are no transformations (and thus the fibers are parallel to main axes)
         conn_type_HR = "parallel_fiber_to_golgi_HR"
         cs_HR = self.scaffold.get_connectivity_set(conn_type_HR)
         self.assertEqual(num_conn, len(cs_HR.connections))
 
-        # Check that half (+- 2) connections are obtained with half the affinity
+        # Check that half (+- 5) connections are obtained with half the affinity
         conn_type_affinity = "parallel_fiber_to_golgi_affinity"
         cs_affinity = self.scaffold.get_connectivity_set(conn_type_affinity)
-        self.assertAlmostEqual(num_conn / 2, len(cs_affinity.connections), delta=4)
+        self.assertAlmostEqual(num_conn / 2, len(cs_affinity.connections), delta=5)
 
-    # def test_quiver_transform(self):
-    #     orientations = []
+        # Check that more connections are obtained when the PC surface is oriented according to orientation vector of 45° rotation in yz plane
+        conn_type_transform = "parallel_fiber_to_golgi_bended"
+        cs_transform = self.scaffold.get_connectivity_set(conn_type_transform)
+        self.assertTrue(len(cs_transform.connections) >= num_conn)
