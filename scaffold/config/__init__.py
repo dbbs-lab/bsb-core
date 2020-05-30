e8772358366333eaffcb0ee93d2ddc01d6d9adfa
Robin De Schepper <robin.deschepper93@gmail.com>
2020-05-30 19:06:08 +0200

    Made scaffold.config a custom module

--- scaffold/config/__init__.py
+++ scaffold/config/__init__.py
@@ -1,1 +1,37 @@
-from ._attrs import attr, list, dict, node, root, dynamic
+import sys, types
+
+_list = list
+from ._attrs import attr, list, dict, node, root, dynamic, ref
+
+_path = __path__
+
+
+class ConfigurationModule(types.ModuleType):
+
+    attr = staticmethod(attr)
+    list = staticmethod(list)
+    dict = staticmethod(dict)
+    node = staticmethod(node)
+    root = staticmethod(root)
+    dynamic = staticmethod(dynamic)
+    ref = staticmethod(ref)
+
+    # The __path__ attribute needs to be retained to mark this module as a package
+    __path__ = _path
+
+    # Load the Configuration class on demand, not on import, to avoid circular
+    # dependencies.
+    _cfg_cls = None
+
+    @property
+    def Configuration(self):
+        if self._cfg_cls is None:
+            from ._config import Configuration
+
+            self._cfg_cls = Configuration
+        return self._cfg_cls
+
+    __all__ = _list(vars().keys() - {"__qualname__", "__module__"})
+
+
+sys.modules[__name__] = ConfigurationModule(__name__)
