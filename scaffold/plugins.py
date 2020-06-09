8ffc3ec2d355d128a5f19e720a70a197c596c240
Robin De Schepper <robin.deschepper93@gmail.com>
2020-06-09 20:38:21 +0200

    Added plugins module

---
+++ scaffold/plugins.py
@@ -0,0 +1,16 @@
+import pkg_resources
+from .exceptions import *
+
+
+def discover(category, *args, **kwargs):
+    plugins = {}
+    for plugin in pkg_resources.iter_entry_points("dbbs_scaffold." + category):
+        try:
+            advert = plugin.load()
+            if hasattr(advert, "__plugin__"):
+                advert = advert.__plugin__
+            advert._scaffold_plugin = plugin
+            plugins[plugin.name] = advert
+        except:
+            raise PluginError("Could not instantiate the `{}` plugin".format(plugin.name))
+    return plugins
