get_soma_trace precision added. Fixed glom to golgi

scaffold/connectivity/connectome/glomerulus_golgi.py
@@ -96,7 +96,11 @@ class ConnectomeGlomerulusGolgi(TouchingConvergenceDivergence):
                         d = np.array(
                             get_distances(
                                 [c.start for c in comps],
-                                [glom_x[gid], glom_y[gid], glom_z[gid]],
+                                [
+                                    glom_x[gid] - golgi_x,
+                                    glom_y[gid] - golgi_y,
+                                    glom_z[gid] - golgi_z,
+                                ],
                             )
                         )
                         d_comps = np.argsort(d)

scaffold/plotting.py
@@ -295,9 +295,11 @@ def get_branch_trace(compartments, offset=[0.0, 0.0, 0.0], color="black", width=
     )
 
 
-def get_soma_trace(soma_radius, offset=[0.0, 0.0, 0.0], color="black", opacity=1):
-    phi = np.linspace(0, 2 * np.pi, num=10)
-    theta = np.linspace(-np.pi / 2, np.pi / 2, num=5)
+def get_soma_trace(
+    soma_radius, offset=[0.0, 0.0, 0.0], color="black", opacity=1, steps=5
+):
+    phi = np.linspace(0, 2 * np.pi, num=steps * 2)
+    theta = np.linspace(-np.pi / 2, np.pi / 2, num=steps)
     phi, theta = np.meshgrid(phi, theta)
 
     x = np.cos(theta) * np.sin(phi) * soma_radius + offset[0]