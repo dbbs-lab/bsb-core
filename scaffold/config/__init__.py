4fe94bb27f1c8b3ccf19a6147a8f5feea21c3416
Robin De Schepper <robin.deschepper93@gmail.com>
2020-06-13 20:56:47 +0200

    Set parser method doc and name

--- scaffold/config/__init__.py
+++ scaffold/config/__init__.py
@@ -48,6 +48,31 @@ class ConfigurationModule:
     __all__ = _list(vars().keys() - {"__init__", "__qualname__", "__module__"})
 
 
+def _parser_method_docs(parser):
+    mod = parser.__module__
+    mod = mod[8:] if mod.startswith("scaffold.") else mod
+    class_role = ":class:`{} <{}.{}>`".format(parser.__name__, mod, parser.__name__)
+    if parser.data_description:
+        descr = " " + parser.data_description
+    else:
+        descr = ""
+    return (
+        "Create a Configuration object from"
+        + descr
+        + " data from an object or file. The data is passed to the "
+        + class_role
+        + """.
+
+        :param file: Path to a file to read the data from.
+        :type file: str
+        :param data: Data object to hand directly to the parser
+        :type data: any
+        :returns: A Configuration
+        :rtype: :class:`Configuration <.conf.Configuration>`
+    """
+    )
+
+
 def parser_factory(parser):
     def parser_method(self, file=None, data=None):
         if file is not None:
@@ -61,6 +86,8 @@ def parser_factory(parser):
         conf._file = file
         return conf
 
+    parser_method.__name__ = "from_" + parser._scaffold_plugin.name
+    parser_method.__doc__ = _parser_method_docs(parser)
     return parser_method
 
 
