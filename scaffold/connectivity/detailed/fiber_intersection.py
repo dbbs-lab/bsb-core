Fix rotation and quiver transform (#439)
* Changed vector for antiparallel morphology rotation

* Changed quiver error for fiber compartments falling outside the quiver volume

* Fixed warn message

* Added warn import

* Added detach branch function

* Added detach call and NaN catch in orientation

* Added orientation in Branch initialization

* Changed detach input to comp from comp id

* Untested logical wizardry

* Fixed terminus name

* Fixed walk branch function

* Added ordered keyword in branch detach

* Attempt to reduce time spent on np.array

* Monitoring print

* Added monitor of cut branches and fiber and final summarizing warning

* Added resolution tolerance

* branch_cut_num as attribute of FiberTransform

* Added init function in FiberTransform

* Added super().__init__ to FiberTransform

* Fixed super position

* Updated name _branch_cut_num

* Removed debug print

Co-authored-by: Robin De Schepper <robin.deschepper93@gmail.com>

scaffold/connectivity/detailed/fiber_intersection.py
@@ -7,6 +7,7 @@ from ...exceptions import *
 from ...helpers import ConfigurableClass
 from ...networks import FiberMorphology, Branch
 from ...plotting import plot_fiber_morphology
+from ...reporting import report, warn
 import abc
 
 # Import rtree
@@ -92,6 +93,7 @@ class FiberIntersection(ConnectionStrategy, MorphologyStrategy):
         morphologies_out = []
 
         fig = None
+        fiber_cut_num = 0
         for c, (from_cell, from_morpho) in enumerate(from_morphology_set):
             # (1) Extract the FiberMorpho object for each branch in the from_compartments
             # of the presynaptic morphology
@@ -118,6 +120,8 @@ class FiberIntersection(ConnectionStrategy, MorphologyStrategy):
                 self.transformation.transform_branches(
                     fm.root_branches, from_cell.position
                 )
+                if self.transformation._branch_cut_num > 0:
+                    fiber_cut_num += 1
 
             if c in self.to_plot:
                 fig = plot_fiber_morphology(fm, fig=fig, offset=from_cell.position)
@@ -226,7 +230,14 @@ class FiberIntersection(ConnectionStrategy, MorphologyStrategy):
                     [from_morpho._set_index, joined_map_offset + to_morpho._set_index]
                 )
                 connections_out.append([from_cell.id, to_cell.id])
-
+        # Throw warning on cut fibers:
+        if fiber_cut_num > 0:
+            warn(
+                "{} fibers out of {} were cut due to outside of quiver volume or external region voxels.".format(
+                    fiber_cut_num, c + 1
+                ),
+                QuiverFieldWarning,
+            )
         self.scaffold.connect_cells(
             self,
             np.array(connections_out or np.empty((0, 2))),
@@ -295,6 +306,10 @@ class FiberIntersection(ConnectionStrategy, MorphologyStrategy):
 
 
 class FiberTransform(ConfigurableClass):
+    def __init__(self):
+        super().__init__()
+        self._branch_cut_num = 0
+
     def transform_branches(self, branches, offset=None):
         if offset is None:
             offset = np.zeros(3)
@@ -330,9 +345,13 @@ class QuiverTransform(FiberTransform):
                 "Attribute 'shared' can't be True for {} transformation".format(self.name)
             )
 
-        # Only QuiverTransform has the attribute quivers, giving the orientation in a discretized volume of size volume_res
+        # Only QuiverTransform has the attribute quivers, giving the orientation in a
+        # discretized volume of size volume_res
         if self.quivers is None:
             raise AttributeError("Missing  attribute 'quivers' for {}".format(self.name))
+
+        if type(self.quivers) is not np.ndarray:
+            self.quivers = np.array(self.quivers)
 
         if not hasattr(self, "vol_res"):
             raise AttributeError("Missing  attribute 'vol_res' for {}".format(self.name))
@@ -358,8 +377,7 @@ class QuiverTransform(FiberTransform):
             :returns: a transformed branch
 
         """
-
-        orientation_data = np.array(self.quivers)
+        orientation_data = self.quivers
         volume_res = self.vol_res
         volume_start = self.vol_start
 
@@ -395,12 +413,22 @@ class QuiverTransform(FiberTransform):
                 if (np.array(voxel_ind) < np.array([0, 0, 0])).all() or (
                     np.array(voxel_ind) > np.array(orientation_data.shape[1:])
                 ).all():
-                    raise QuiverFieldError(
-                        "The reconstructed volume is bigger than the quiver field volume. Selected voxel does not have an associated quiver"
-                    )
+                    # Update number of cut branches
+                    self._branch_cut_num += 1
+                    # Detach subsequent compartments from branch
+                    leftover_branch = branch.detach(branch._compartments[comp])
+                    break
+
                 orientation_vector = orientation_data[
                     :, voxel_ind[0], voxel_ind[1], voxel_ind[2]
                 ]
+
+                # Catch values belonging to a different area than the reconstructed one (marked by NaN)
+                if np.isnan(orientation_vector).any():
+                    self._branch_cut_num += 1
+                    # Detach subsequent compartments from branch
+                    leftover_branch = branch.detach(branch._compartments[comp])
+                    break
 
                 cross_prod = np.cross(orientation_vector, transversal_vector)
                 cross_prod = cross_prod / np.linalg.norm(cross_prod)

scaffold/morphologies.py
@@ -466,11 +466,13 @@ def get_rotation_matrix(v0, v):
             ),
             level=3,
         )
-        # We will rotate the morphology of 180° around a vector orthogonal to the starting vector v0 (the same would be if we take the ending vector v)
-        # We set the first and second components to 1; the third one is obtained to have the scalar product with v0 equal to 0
+        # We will rotate the morphology of 180° around a vector orthogonal to
+        # the starting vector v0 (the same would be if we take the ending vector
+        # v). We set the first and third components to 1; the second one is
+        # obtained to have the scalar product with v0 equal to 0
         kx = 1
-        ky = 1
-        kz = -(v0[0] + v0[1]) / v0[2]
+        kz = 1
+        ky = -(v0[0] + v0[2]) / v0[1]
         k = np.array([kx, ky, kz])
     else:
         k = (np.cross(v0, v)) / math.sin(alpha)

scaffold/networks.py
@@ -103,6 +103,7 @@ class Branch:
         while start._child is not None:
             yield start
             start = start._child
+        yield start
 
     def append(self, compartment):
         self._compartments.append(compartment)
@@ -113,7 +114,7 @@ class Branch:
     def interpolate(self, resolution):
         for comp in self._compartments:
             length_comp = np.linalg.norm(comp.end - comp.start)
-            if length_comp > resolution:
+            if length_comp > resolution + 1e-3:
                 num_to_add = math.ceil(length_comp / resolution)
                 added_points = self.split(comp, num_to_add)
 
@@ -177,6 +178,23 @@ class Branch:
             # branch iteration starts from the new compartment.
             self._root = new_compartments[0]
 
+    def detach(self, compartment):
+        if compartment is self._root or compartment._parent is None:
+            # We have been asked to detach ourselves from ourselves: that just yields our whole branch again
+            return self
+        # Detach our parent from the cutoff point.
+        compartment._parent._child = None
+        self._terminus = compartment._parent
+        # Detach ourself from our parent.
+        compartment._parent = None
+        detached_branch = Branch(
+            list(self.walk(start=compartment)), self.orientation, ordered=False
+        )
+        # Clean up all detached compartments that now belong to the detached branch
+        for detached_comp in detached_branch.walk():
+            self._compartments.remove(detached_comp)
+        return detached_branch
+
     def voxelize(self, position, bounding_box, voxel_tree, map, voxel_list):
         v_id = len(voxel_list)
         for v, comp in enumerate(self._compartments):
@@ -221,10 +239,27 @@ def _get_root_compartment(compartment, compartments):
     return compartment
 
 
+def _get_single_child(compartment, compartments):
+    # Get the child either by the _child attribute, or the only element of _children
+    child = (
+        hasattr(compartment, "_child")
+        and compartment._child
+        or hasattr(compartment, "_children")
+        and len(compartment._children) == 1
+        and compartment._children[0]
+    )
+    # If a child was found this way, check if it is in the compartments array and return
+    # it, otherwise this function returns False
+    return child and child in compartments and child
+
+
 def _get_terminal_compartment(compartment, compartments):
-    while len(compartment._children) == 1 and compartment._children[0] in compartments:
-        compartment = compartment._children[0]
-    return compartment
+    result = compartment
+    child = _get_single_child(compartment, compartments)
+    while child:
+        result = child
+        child = _get_single_child(compartment, compartments)
+    return result
 
 
 def _consume_branch(unvisited, root_compartment, orientation, parent=None):