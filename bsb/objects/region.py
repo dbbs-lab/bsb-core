231aae20a3433a54594d074de7a6741ba729d2b3
Robin De Schepper <robin.deschepper93@gmail.com>
2020-09-11 11:07:49 +0200

    fixed scaffold reference

--- bsb/objects/region.py
+++ bsb/objects/region.py
@@ -2,7 +2,7 @@ from .. import config
 from ..config import types
 
 # Create a dynamic class that by default dynamically casts to this class.
-@config.dynamic(required=False, default="scaffold.objects.Region")
+@config.dynamic(required=False, default="bsb.objects.Region")
 class Region:
     name = config.attr(key=True)
     origin = config.attr(type=types.list(type=float, size=3), required=True)
