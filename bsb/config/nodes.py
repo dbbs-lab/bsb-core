972f628b2296a6e131dcd6fbd89a2adb54d58d23
Robin De Schepper <robin.deschepper93@gmail.com>
2020-09-11 17:38:56 +0200

    Added Distribution node

--- bsb/config/nodes.py
+++ bsb/config/nodes.py
@@ -1,6 +1,11 @@
-from . import attr, list, dict, node, root, pluggable, slot
+from . import attr, list, dict, node, root, pluggable, slot, catch_all
 from . import types
 from .. import plugins
+import scipy.stats.distributions as _distributions
+
+_available_distributions = [
+    d for d in dir(_distributions) if not d.startswith("_") and not d.endswith("_gen")
+]
 
 
 @pluggable(key="engine", plugin_name="storage engine", unpack=lambda p: p.StorageNode)
@@ -18,3 +23,14 @@ class StorageNode:
 class NetworkNode:
     x = attr(type=float, required=True)
     z = attr(type=float, required=True)
+
+
+@node
+class Distribution:
+    distribution = attr(type=types.in_(_available_distributions), required=True)
+    parameters = catch_all(type=types.any())
+
+    def draw(self, n):
+        if not hasattr(self, "_distr"):
+            self._distr = getattr(_distributions, self.distribution)(**self.parameters)
+        return self._distr.rvs(size=n)
