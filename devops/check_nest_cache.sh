1507d98e5f4c27a409ba19fc7834a1d7c663342a
Robin De Schepper <robin.deschepper93@gmail.com>
2020-09-13 20:51:29 +0200

    Fixed devops script not finding nest cache

--- devops/check_nest_cache.sh
+++ devops/check_nest_cache.sh
@@ -1,4 +1,4 @@
-if [ ! -d /home/travis/nest-$NEST_VERSION/lib/python3.6 ] ; then
+if [ ! -d /home/travis/nest-$NEST_VERSION/lib/python3.8 ] ; then
   export HAS_NEST_CACHE=0
 else
   export HAS_NEST_CACHE=1
