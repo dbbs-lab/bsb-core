4d527a4de2d1bd1f5fbab5bd073ceed379598c5f
Robin De Schepper <robin.deschepper93@gmail.com>
2020-09-13 19:51:42 +0200

    Polished IO-PC: there was excessive shuffling and there were no clusters

--- bsb/connectivity/connectome/io_purkinje.py
+++ bsb/connectivity/connectome/io_purkinje.py
@@ -7,26 +7,16 @@ from ... import config
 class ConnectomeIOPurkinje(ConnectionStrategy):
     """
         Legacy implementation for the connection between inferior olive and Purkinje cells.
-        Purkinje cells are clustered (number of clusters is the number of IO cells), and each clusters
-        is innervated by 1 IO cell
     """
 
     def validate(self):
         pass
 
     def connect(self):
-        from_type = self.presynaptic.type
-        to_type = self.postsynaptic.type
-        io_cells = self.from_cells[from_type.name]
-        if len(io_cells) == 0:
-            self.scaffold.connect_cells(self, np.empty((0, 2)))
-            return
-        purkinje_cells = self.to_cells[to_type.name]
-        convergence = 1  # Purkinje cells should be always constrained to receive signal from only 1 Inferior Olive neuron
+        io_cells = self.from_cells[self.presynaptic.type.name]
+        purkinje_cells = self.to_cells[self.postsynaptic.type.name]
         io_purkinje = np.empty([len(purkinje_cells), 2])
-        for i, pc in enumerate(purkinje_cells):
-            np.random.shuffle(io_cells)
-            io_purkinje[i, 0] = io_cells[0, 0]
-            io_purkinje[i, 1] = pc[0]
-        results = io_purkinje
-        self.scaffold.connect_cells(self, results)
+        random_io_indices = np.random.randint(len(io_cells), size=len(purkinje_cells))
+        io_purkinje[:, 0] = io_cells[random_io_indices, 0]
+        io_purkinje[:, 1] = purkinje_cells[:, 0]
+        self.scaffold.connect_cells(self, io_purkinje)
