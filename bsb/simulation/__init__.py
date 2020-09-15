75195c78eced5f7b5810778884f369c92d2f50a4
Robin De Schepper <robin.deschepper93@gmail.com>
2020-09-15 14:11:26 +0200

    TargetsNeuron became NeuronTargetting in v4

--- bsb/simulation/__init__.py
+++ bsb/simulation/__init__.py
@@ -2,5 +2,5 @@ from .component import SimulationComponent
 from .cell import CellModel
 from .connection import ConnectionModel
 from .device import DeviceModel
-from .targetting import TargetsNeurons, TargetsSections
+from .targetting import NeuronTargetting, TargetsSections
 from .adapter import SimulatorAdapter
