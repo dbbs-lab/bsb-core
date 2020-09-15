902f915ce837d166c104ea3f260044c3d63a5adb
Robin De Schepper <robin.deschepper93@gmail.com>
2020-09-15 09:35:24 +0200

    Started on parsing tests

--- tests/test_parsing.py
+++ tests/test_parsing.py
@@ -6,22 +6,30 @@ from bsb.config import from_json
 from bsb.exceptions import *
 
 
-def relative_to_tests_folder(path):
-    return os.path.join(os.path.dirname(__file__), path)
+def p(file):
+    return os.path.join(os.path.dirname(__file__), "parser_tests", file)
 
 
-minimal_config = relative_to_tests_folder("configs/test_minimal.json")
-full_config = relative_to_tests_folder("configs/test_full_v4.json")
-
-
-def as_json(f):
-    import json
-
-    with open(f, "r") as fh:
-        return json.load(fh)
+def c(f):
+    with open(p(f), "r") as fh:
+        return fh.read()
 
 
 class TestConfiguration(unittest.TestCase):
     def test_get_parser(self):
         config.get_parser("json")
         self.assertRaises(PluginError, config.get_parser, "doesntexist")
+
+    def test_parse_empty_doc(self):
+        tree, meta = config.get_parser("json").parse(c("doc.json"))
+        self.assertEqual({}, tree, "'doc.json' parse should produce empty dict")
+
+    def test_parse_basics(self):
+        tree, meta = config.get_parser("json").parse(c("basics.json"))
+        print(dict(tree))
+        self.assertEqual(3, tree["list"][2], "Incorrectly parsed basic JSON")
+        self.assertEqual(
+            "just like that",
+            tree["nest me hard"]["oh yea"],
+            "Incorrectly parsed nested JSON",
+        )
