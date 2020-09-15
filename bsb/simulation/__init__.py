0ca1d633baaf8a3e5aa0e5f303b3c0dfbc37fb5a
Robin De Schepper <robin.deschepper93@gmail.com>
2020-09-15 12:53:12 +0200

    uncluttered simulation module

--- bsb/simulation/__init__.py
+++ bsb/simulation/__init__.py
@@ -1,8 +1,3 @@
-import abc, random, types
-import numpy as np
-from ..helpers import ConfigurableClass, SortableByAfter
-from ..reporting import report
-from ..exceptions import *
 from .component import SimulationComponent
 from .cell import CellModel
 from .connection import ConnectionModel
