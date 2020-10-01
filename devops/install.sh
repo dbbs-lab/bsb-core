5a882b92a9011f5447714f7fc19d19a8471bb125
Robin De Schepper <robin.deschepper93@gmail.com>
2020-10-01 12:14:45 +0200

    Refactored into install script. Check if errors propagate.

---
+++ devops/install.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+
+export NEST_INSTALL_DIR=/home/travis/nest-$NEST_VERSION
+wget https://neuron.yale.edu/ftp/neuron/versions/v7.7/nrn-7.7.x86_64-linux.deb
+sudo dpkg -i nrn-7.7.x86_64-linux.deb
+export PYTHONPATH=/usr/local/nrn/lib/python:$PYTHONPATH
+source devops/check_nest_cache.sh
+sudo apt-get install -y python3-rtree python3.8-dev build-essential cmake cython libgsl-dev libltdl-dev libncurses-dev libreadline-dev openmpi-bin libopenmpi-dev
+if [ "$HAS_NEST_CACHE" = "0" ]; then
+  source devops/install_nest.sh
+else
+  echo "NEST cache found, skipping installation";
+fi
+if [ "$HAS_CEREBNEST_CACHE" = "0" ]; then
+  source devops/install_nest_modules.sh
+else
+  echo "CEREBNEST cache found, skipping installation";
+fi
+source devops/post_install_env_vars.sh
+python -c "import nest; nest.Install('cerebbmodule')"
+pip3 install --upgrade pip
+pip3 install -r requirements.txt
+pip3 install coverage
+pip3 install -e .
