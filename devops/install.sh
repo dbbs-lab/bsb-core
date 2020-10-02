c1985a36fb44f484540698da7e9e15dbb8bd4c38
Robin De Schepper <robingilbert.deschepper@unipv.it>
2020-10-02 13:59:44 +0200

    Error sensitive install of NEST & NEURON on Travis pipeline (#117)
    * Install python3-dev for Python.h?
    
    * install specific 3.8 dev package.
    
    * try to let nest handle finding of libs etc
    
    * use NEST's travis build script
    
    * Fixed cmake dir
    
    * more prints
    
    * find python libs
    
    * fixed find
    
    * find everywhere
    
    * found lib?
    
    * install newer cython
    
    * Use the DBBS NEST fork
    
    * maybe with new commit a new cache?
    
    * really?
    
    * fuck the cache-lice
    
    * removed branch specification
    
    * Revert "fuck the cache-lice"
    
    This reverts commit 066609b614948e1b50defea1d32dce1e27b5a584.
    
    * check nest install & module before continuing
    
    * Refactored into install script. Check if errors propagate.
    
    * check if travis_terminate helps end the pipeline on error
    
    * fixed deps
    
    * Use `errexit` to err out when a command returns non true value
    
    * load nest env vars before checking nest install
    
    * all signals go
    
    * Added NEURON check
    
    * Install NEURON through pip
    
    * check python env from inside install script
    
    * fixed missing pip module in pip install commands
    
    * Install NEURON v7.8.1.1

---
+++ devops/install.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+export NEST_INSTALL_DIR=/home/travis/nest-$NEST_VERSION
+python -m pip install --upgrade pip
+python -m pip install NEURON==7.8.1.1
+source devops/check_nest_cache.sh
+sudo apt-get install -y
+if [ "$HAS_NEST_CACHE" = "0" ]; then
+  source devops/install_nest.sh
+else
+  echo "NEST cache found, skipping installation";
+fi
+if [ "$HAS_CEREBNEST_CACHE" = "0" ]; then
+  source devops/install_nest_modules.sh
+else
+  echo "CEREBNEST cache found, skipping installation";
+fi
+source devops/post_install_env_vars.sh
+python -c "import nest; nest.Install('cerebmodule')"
+python -c "import neuron; neuron.test()"
+python -m pip install -r requirements.txt
+python -m pip install coverage
+python -m pip install -e .
