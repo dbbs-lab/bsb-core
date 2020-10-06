36ffd3c5860cca75b1b2f6c7cd27d41aca7b6ef2
Robin De Schepper <robin.deschepper93@gmail.com>
2020-10-06 11:25:16 +0200

    Fixed postprocessing test

--- tests/test_postprocessing.py
+++ tests/test_postprocessing.py
@@ -63,16 +63,10 @@ class TestPostProcessing(unittest.TestCase):
         try:
             i = cs.intersections
             for inter in i:
-                ft = inter.from_compartment.type
-                tt = inter.to_compartment.type
-                self.assertTrue(
-                    ft == 2 or ft >= 200 and ft < 300,
-                    "From compartment type is not an axon",
-                )
-                self.assertTrue(
-                    tt == 3 or tt >= 300 and tt < 400,
-                    "To compartment type is not a dendrite",
-                )
+                fl = inter.from_compartment.labels
+                tl = inter.to_compartment.labels
+                self.assertIn("axon", fl, "From compartment type is not an axon")
+                self.assertIn("dendrites", tl, "From compartment type is not a dendrite")
             self.assertNotEqual(len(i), 0, "Empty intersection data")
             self.assertEqual(
                 len(i), original_connections, "Different amount of spoofed connections"
