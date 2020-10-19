4d4ef319ae78fec796212e127cdac9d66f8c74ce
claudia casellato <claudia.casellato@unipv.it>
2020-10-19 15:42:06 +0200

    MissingAxon hook now handles missing intersections. closes #46.

--- bsb/postprocessing.py
+++ bsb/postprocessing.py
@@ -220,24 +220,9 @@ class SpoofDetails(PostProcessingHook):
         #
         # The left column will be the first from_morphology (0) and the right column
         # will be the first to_morphology (1)
-        morphologies = np.column_stack(
-            (
-                np.zeros(
-                    (
-                        len(
-                            connectivity_matrix,
-                        )
-                    )
-                ),
-                np.ones(
-                    (
-                        len(
-                            connectivity_matrix,
-                        )
-                    )
-                ),
-            )
-        )
+        _from = np.zeros(len(connectivity_matrix))
+        _to = np.ones(len(connectivity_matrix))
+        morphologies = np.column_stack((_from, _to))
         # Generate the map
         morpho_map = [from_morphologies[0], to_morphologies[0]]
         from_m = self.scaffold.morphology_repository.get_morphology(from_morphologies[0])
@@ -278,5 +263,11 @@ class MissingAxon(PostProcessingHook):
             for type in self.types:
                 if ct.from_cell_types[0].name == type:
                     for tag in ct.tags:
+                        if tag not in self.scaffold.connection_compartments:
+                            warn(
+                                f"MissingAxon hook skipped {tag}, missing detailed intersection data.",
+                                ConnectivityWarning,
+                            )
+                            continue
                         compartment_matrix = self.scaffold.connection_compartments[tag]
                         compartment_matrix[:, 0] = np.zeros(len(compartment_matrix))
