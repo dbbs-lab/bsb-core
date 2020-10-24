60d3018dcbc84a2a8c2d5d816bb0d16aa0b38507
Robin De Schepper <robin.deschepper93@gmail.com>
2020-10-24 15:08:44 +0200

    documented the hook system

--- bsb/config/_hooks.py
+++ bsb/config/_hooks.py
@@ -3,24 +3,70 @@ def _dunder(*hooks):
 
 
 def run_hook(obj, hook, *args, **kwargs):
+    """
+    Execute the ``hook`` hook of ``obj``.
+
+    Runs the ``hook`` method ``obj`` but also looks through the class hierarchy for
+    essential hooks with the name ``__<hook>__``.
+
+    .. note::
+
+      Essential hooks are only ran if the method is called using ``run_hook`` while
+      non-essential hooks are wrapped around the method and will always be executed when
+      the method is called (see https://github.com/dbbs-lab/bsb/issues/158).
+    """
+    # Traverse the MRO tree and execute all essential (__hook__) methods.
     for parent in reversed(obj.__class__.__mro__):
         if hasattr(parent, _dunder(hook)):
             getattr(parent, _dunder(hook))(obj, *args, **kwargs)
+    # Execute the final regular hook method.
     if hasattr(obj, hook):
         getattr(obj, hook)(*args, **kwargs)
 
 
 def on(hook, cls, essential=False, before=False):
+    """
+    Register a class hook.
+
+    :param hook: Name of the method to hook.
+    :type hook: str
+    :param cls: Class to hook.
+    :type cls: class
+    :param essential: If the hook is essential, it will always be executed even in child classes that override the hook. Essential hooks are only lost if the method on ``cls`` is replaced.
+    :type essential: bool
+    :param before: If ``before`` the hook is executed before the method, otherwise afterwards.
+    :type before: bool
+    """
     if essential:
         hook = _dunder(hook)
     return _hook(cls, hook, before)
 
 
 def after(hook, cls, essential=False):
+    """
+    Register a class hook to run after the target method.
+
+    :param hook: Name of the method to hook.
+    :type hook: str
+    :param cls: Class to hook.
+    :type cls: class
+    :param essential: If the hook is essential, it will always be executed even in child classes that override the hook. Essential hooks are only lost if the method on ``cls`` is replaced.
+    :type essential: bool
+    """
     return on(hook, cls, essential=essential, before=False)
 
 
 def before(hook, cls, essential=False):
+    """
+    Register a class hook to run before the target method.
+
+    :param hook: Name of the method to hook.
+    :type hook: str
+    :param cls: Class to hook.
+    :type cls: class
+    :param essential: If the hook is essential, it will always be executed even in child classes that override the hook. Essential hooks are only lost if the method on ``cls`` is replaced.
+    :type essential: bool
+    """
     return on(hook, cls, essential=essential, before=True)
 
 
@@ -33,14 +79,34 @@ def _super(cls, attr):
 
 
 def has_hook(instance, hook):
+    """
+    Checks the existence of a method or essential method on the ``instance``.
+
+    :param instance: Object to inspect.
+    :type instance: object
+    :param hook: Name of the hook to look for.
+    :type hook: str
+    """
     return hasattr(instance, hook) or hasattr(instance, _dunder(hook))
 
 
 def overrides(cls, hook):
+    """
+    Returns ``True`` if a class has implemented a method or ``False`` if it has inherited
+    it.
+
+    :param cls: Class to inspect.
+    :type cls: class
+    :param hook: Name of the hook to look for.
+    :type hook: str
+    """
     return hasattr(cls, hook) and getattr(cls, hook) is not _super(cls, hook)
 
 
 def _hook(cls, hook, before):
+    # Returns a decorator that will wrap the existing implementation if it exists,
+    # otherwise returns a decorator that will simply add the decorated function to the
+    # class.
     if overrides(cls, hook):
         return _make_wrapper(cls, hook, before)
     else:
@@ -49,7 +115,8 @@ def _hook(cls, hook, before):
 
 def _make_injector(cls, hook):
     def decorator(func):
-        setattr(cls, hook, func)
+        # Set the decorated function as the hooked function.
+        _set_hooked_func(cls, hook, func)
 
     return decorator
 
@@ -67,19 +134,20 @@ def _make_wrapper(cls, hook, before):
 
     def decorator(func):
         if before:
-
+            # Wrapper that executes the decorated function before the class method
             def hooked(*args, **kwargs):
                 func(*args, **kwargs)
                 r = func_to_hook(*args, **kwargs)
                 return r
 
         else:
-
+            # Wrapper that executes the decorated function after the class method
             def hooked(*args, **kwargs):
                 r = func_to_hook(*args, **kwargs)
                 func(*args, **kwargs)
                 return r
 
+        # Set the wrapper function as the hooked function
         _set_hooked_func(cls, hook, hooked)
 
     return decorator
---
+++ docs/source/config/module/hooks.rst
@@ -0,0 +1,75 @@
+###################
+Configuration hooks
+###################
+
+The BSB provides a small and elegant hook system. The system allows the user to hook
+methods of classes. It is intended to be a hooking system that requires bidirectional
+cooperation: the developer declares which hooks they provide and the user is supposed to
+only hook those functions. Using the hooks in other places will behave slightly different,
+see the note on `wild hooks`_.
+
+For a list of BSB endorsed hooks see :doc:`list_of_hooks`.
+
+=============
+Calling hooks
+=============
+
+A developer can call the user-registered hook using :func:`bsb.config.run_hook`:
+
+.. code-block:: python
+
+  import bsb.config
+
+  bsb.config.run_hook(instance, "my_hook")
+
+This will check the class of instance and all of its parent classes for implementations of
+``__my_hook__`` and execute them in closest relative first order, starting from the class
+of ``instance``. These ``__my_hook_`` methods are known as `essential hooks`_.
+
+============
+Adding hooks
+============
+
+Hooks can be added to class methods using the :func:`bsb.config.on` decorator (or
+:func:`bsb.config.before`/:func:`bsb.config.after`). The decorated function will then be
+hooked onto the given class:
+
+.. code-block:: python
+
+  from bsb import config
+  from bsb.core import Scaffold
+  from bsb.simulation import Simulation
+
+  @config.on(Simulation, "boot")
+  def print_something(self):
+    print("We're inside of `Simulation`'s `boot` hook!")
+    print(f"The {self.name} simulation uses {self.simulator}.")
+
+  cfg = config.Configuration.default()
+  cfg.simulations["test"] = Simulation(simulator="nest", ...)
+  scaffold = Scaffold(cfg)
+  # We're inside of the `Simulation`s `boot` hook!
+  # The test simulation uses nest.
+
+
+===============
+Essential hooks
+===============
+
+Essential hooks are those that follow Python's "magic method" convention (``__magic__``).
+Essential hooks allow parent classes to execute hooks even if child classes override the
+direct ``my_hook`` method. After executing these essential hooks ``instance.my_hook`` is
+called which will contain all of the non-essential class hooks. Unlike non-essential hooks
+they are not run whenever the hooked method is executed but only when the hooked method is
+invoked through
+
+==========
+Wild hooks
+==========
+
+Since the non-essential hooks are wrappers around the target method you could use the
+hooking system to hook methods of classes that aren't ever invoked as a hook, but still
+used during the operation of the class and your hook will be executed anyway. You could
+even use the hooking system on any class not part of the BSB at all. Just keep in mind
+that if you place an essential hook onto a target method that's never explicitly invoked
+as a hook that it will never run at all.
