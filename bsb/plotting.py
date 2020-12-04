Added correct computation of total cell number for PSTH

bsb/plotting.py
@@ -871,7 +871,9 @@ class PSTHRow:
 
 
 @_figure
-def hdf5_plot_psth(handle, duration=3, cutoff=0, start=0, fig=None, mod=None, **kwargs):
+def hdf5_plot_psth(
+    handle, scaffold_instance, duration=3, cutoff=0, start=0, fig=None, mod=None, **kwargs
+):
     psth = PSTH()
     row_map = {}
     for g in handle.values():
@@ -907,20 +909,30 @@ def hdf5_plot_psth(handle, duration=3, cutoff=0, start=0, fig=None, mod=None, **
     # should happen before this point.
     fig._grid_ref = subplots_fig._grid_ref
     fig._layout = subplots_fig._layout
+    cell_types = scaffold_instance.get_cell_types()
     for i, row in enumerate(psth.ordered_rows()):
         for name, stack in sorted(row.stacks.items(), key=lambda x: x[0]):
             counts, bins = np.histogram(stack.list, bins=np.arange(start, _max, duration))
+            # Workaround of Workarounds for merging info in scaffold and in results
+            current_cell = list(
+                filter(
+                    lambda cell_type: cell_type.plotting.color.lower() == stack.color,
+                    list(cell_types),
+                )
+            )
+            cell_num = scaffold_instance.get_placed_count(current_cell[0].name)
             if str(name).startswith("##"):
                 # Lazy way to order the stacks; Stack names can start with ## and a number
                 # and it will be sorted by name, but the ## and number are not displayed.
                 name = name[4:]
             trace = go.Bar(
                 x=bins,
-                y=counts / stack.cells * 1000 / duration,
+                y=counts / cell_num * 1000 / duration,
                 name=name or row.name,
                 marker=dict(color=stack.color),
             )
             fig.add_trace(trace, row=i + 1, col=1)
+
     return fig
 
 