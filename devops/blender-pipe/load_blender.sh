751c1929567aaf2cc18b88bad14a73483dd07f69
Robin De Schepper <robin.deschepper93@gmail.com>
2021-01-26 13:47:31 +0100

    Updated blender devops scripts

---
+++ devops/blender-pipe/load_blender.sh
@@ -0,0 +1,10 @@
+module load daint-mc Mesa/.18.3.3-CrayGNU-20.08
+module load gcc/9.3.0
+# Multicore stack
+module load daint-mc
+# Python 3 & mpi4py
+module load cray-python/3.8.2.1
+# Cython
+module load PyExtensions/python3-CrayGNU-20.08
+# h5py
+module load h5py/2.10.0-CrayGNU-20.08-python3-parallel
