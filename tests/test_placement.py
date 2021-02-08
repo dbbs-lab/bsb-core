ab60ea08b123382a33396690bc29fdc3519f3f6e
Robin De Schepper <robin.deschepper93gmail.com>
2021-02-08 18:14:57 +0100

    Test running 2 pools in a row, master loop, fake futures & dependencies

--- tests/test_placement.py
+++ tests/test_placement.py
@@ -4,7 +4,7 @@ from mpi4py import MPI
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
 from bsb.core import Scaffold
 from bsb.config import Configuration
-from bsb._pool import JobPool
+from bsb._pool import JobPool, FakeFuture
 from time import sleep
 
 
@@ -25,7 +25,52 @@ class SchedulerBaseTest:
     MPI.COMM_WORLD.Get_size() < 2, "Skipped during serial testing."
 )
 class TestParallelScheduler(unittest.TestCase, SchedulerBaseTest):
-    pass
+    def test_double_pool(self):
+        network = Scaffold()
+        pool = JobPool(network)
+        job = pool.queue(test_dud, (5, 2))
+        pool.execute()
+        pool = JobPool(network)
+        job = pool.queue(test_dud, (5, 2))
+        pool.execute()
+
+    def test_master_loop(self):
+        network = Scaffold()
+        pool = JobPool(network)
+        job = pool.queue(test_dud, (5, 2))
+        executed = False
+        def spy_loop(p):
+            nonlocal executed
+            executed = True
+        pool.execute(master_event_loop=spy_loop)
+        if MPI.COMM_WORLD.Get_rank():
+            self.assertFalse(executed, 'workers executed master loop')
+        else:
+            self.assertTrue(executed, 'master loop skipped')
+
+    def test_fake_futures(self):
+        network = Scaffold()
+        pool = JobPool(network)
+        job = pool.queue(test_dud, (5, 2))
+        self.assertIs(FakeFuture.done, job._future.done.__func__)
+        self.assertFalse(job._future.done())
+        self.assertFalse(job._future.running())
+
+    def test_dependencies(self):
+        network = Scaffold()
+        pool = JobPool(network)
+        job = pool.queue(test_dud, (5, 2))
+        job2 = pool.queue(test_dud, (5, 2), deps=[job])
+        result = None
+        def spy_queue(jobs):
+            nonlocal result
+            if result is None:
+                result = jobs[0]._future.running() and not jobs[1]._future.running()
+
+        pool.execute(master_event_loop=spy_queue)
+        if not MPI.COMM_WORLD.Get_rank():
+            self.assertTrue(result, 'A job with unfinished dependencies was scheduled.')
+
 
 
 @unittest.skipIf(
