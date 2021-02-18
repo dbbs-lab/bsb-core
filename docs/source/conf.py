62e32dd7c9cac8f88046b58d988e1e59d21cd3ac
Robin De Schepper <robin.deschepper93@gmail.com>
2021-02-18 18:12:21 +0100

    added logo & favicon

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -120,6 +120,9 @@ html_context = {
     "projects": {"DBBS Scaffold": "https://github.com/dbbs/bsb"},
 }
 
+html_logo = "../images/bsb.png"
+html_favicon = "../images/bsb_ico.png"
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
