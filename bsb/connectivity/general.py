6f56cbad88f109a9f94929c5b6f49c73692d3f31
Robin De Schepper <robin.deschepper93gmail.com>
2021-03-17 11:24:20 +0100

    fixed mapping

--- bsb/connectivity/general.py
+++ bsb/connectivity/general.py
@@ -97,8 +97,10 @@ class ExternalConnections(ConnectionStrategy):
         )
         if self.use_map:
             emap = lambda t: t.placement.name + "_ext_map"
-            from_gid_map = self.scaffold.load_appendix(emap(from_type))
-            to_gid_map = self.scaffold.load_appendix(emap(to_type))
-            data[0, :] = np.vectorize(from_gid_map.get)(data[0, :])
-            data[1, :] = np.vectorize(to_gid_map.get)(data[1, :])
+            froms = self.scaffold.load_appendix(emap(from_type))
+            from_map = dict(zip(froms[:, 0], froms[:, 1]))
+            tos = self.scaffold.load_appendix(emap(to_type))
+            to_map = dict(zip(tos[:, 0], tos[:, 1]))
+            data[0, :] = np.vectorize(from_map.get)(data[0, :])
+            data[1, :] = np.vectorize(to_map.get)(data[1, :])
         self.scaffold.connect_cells(self, data)
