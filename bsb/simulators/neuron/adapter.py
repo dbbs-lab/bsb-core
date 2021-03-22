879fd7086ee7448df94478ef918efed3eb2676fd
Robin De Schepper <robin.deschepper93@gmail.com>
2021-03-22 18:09:57 +0100

    b4 - fixed empty time vector & incorrect spike recorder shape

--- bsb/simulators/neuron/adapter.py
+++ bsb/simulators/neuron/adapter.py
@@ -355,6 +355,8 @@ class NeuronAdapter(SimulatorAdapter):
         from plotly.subplots import make_subplots
 
         pc = simulator.parallel
+        # Access the `time` singleton property to activate it.
+        self.h.time
         self.pc = pc
         pc.barrier()
         report("Simulating...", level=2)
