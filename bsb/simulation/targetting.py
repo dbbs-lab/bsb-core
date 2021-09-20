fb519ef6b65cc0bfc7d14f78cc34eb3b6b5219d2
Robin De Schepper <robin.deschepper93@gmail.com>
2021-09-20 11:06:51 +0200

    Breaking changes to section targetting, renamed `section_type`
    * broke default behavior of `TargetsSections` to target all not soma
    
    * fix targetting
    
    * section targetting fixed
    
    * updated config to use the new `section_types`

--- bsb/simulation/targetting.py
+++ bsb/simulation/targetting.py
@@ -211,11 +211,17 @@ class TargetsSections:
 
     def _section_target_default(self, cell):
         if not hasattr(self, "section_count"):
-            self.section_count = 1
+            self.section_count = "all"
+        elif self.section_count != "all":
+            self.section_count = int(self.section_count)
+        sections = cell.sections
+        if hasattr(self, "section_types"):
+            ts = self.section_types
+            sections = [s for s in sections if any(t in s.labels for t in ts)]
         if hasattr(self, "section_type"):
-            sections = [s for s in cell.sections if self.section_type in s.labels]
-        else:
-            sections = cell.soma
+            raise ConfigurationError(
+                "`section_type` is deprecated, use `section_types` instead."
+            )
         if self.section_count == "all":
             return sections
         return [random.choice(sections) for _ in range(self.section_count)]
