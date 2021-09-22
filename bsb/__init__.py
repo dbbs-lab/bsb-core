8371b2febe93929a2434165de5d8d0b706d08d6c
Robin De Schepper <robin.deschepper93@gmail.com>
2021-09-22 12:10:51 +0200

    dev2 - fix `functools.cache`

--- bsb/__init__.py
+++ bsb/__init__.py
@@ -1,3 +1,11 @@
-__version__ = "3.9.0dev1"
+__version__ = "3.9.0dev2"
+
+import functools
+
+# Patch functools on 3.8
+try:
+    functools.cache
+except AttributeError:
+    functools.cache = functools.lru_cache
 
 from .reporting import set_verbosity, report, warn
