4252153c1175531b74c093c2c6e52244282ed955
Robin De Schepper <robin.deschepper93@gmail.com>
2021-09-29 13:01:56 +0200

    removed old matrices from `init_scaffold`

--- bsb/output.py
+++ bsb/output.py
@@ -10,7 +10,7 @@ from numpy import string_
 from .exceptions import *
 from .models import ConnectivitySet, PlacementSet
 from sklearn.neighbors import KDTree
-import os, sys
+import os, sys, functools
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", "..", "dbbs-models"))
 
@@ -747,22 +747,18 @@ class HDF5Formatter(OutputFormatter, MorphologyRepository):
         return os.path.exists(self.file)
 
     def init_scaffold(self):
-        with self.load() as resource:
-            for cell_type_name, count in resource()[
-                "statistics/cells_placed"
-            ].attrs.items():
-                self.scaffold.statistics.cells_placed[cell_type_name] = count
-            for tag in resource()["cells/connections"]:
-                dataset = resource()["cells/connections/" + tag]
+        scf = self.scaffold
+        with self.load() as res:
+            for cell_type_name, count in res()["statistics/cells_placed"].attrs.items():
+                scf.statistics.cells_placed[cell_type_name] = count
+            for tag in res()["cells/connections"]:
+                dataset = res()["cells/connections/" + tag]
                 for contributing_type in dataset.attrs["connection_types"]:
-                    self.scaffold.configuration.connection_types[
-                        contributing_type
-                    ].tags.append(tag)
-            self.scaffold.labels = {
-                l: resource()["cells/labels/" + l][()] for l in resource()["cells/labels"]
-            }
-            hdf5_ids = np.array(resource()["cells/positions"])[:, 0]
-            self.scaffold._nextId = int(np.max(hdf5_ids))
+                    scf.configuration.connection_types[contributing_type].tags.append(tag)
+            scf.labels = {l: v[()] for l, v in res()["cells/labels"].items()}
+            sets = (v["identifiers"] for v in res()["cells/placement"].values())
+            max_ids = (id[::2] + id[1::2] if len(id) else [0] for id in sets)
+            scf._nextId = functools.reduce(max, map(np.max, max_ids), 0)
 
     def validate(self):
         pass
