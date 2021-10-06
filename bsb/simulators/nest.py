2c00260860f419cf6a6e56676999525f7df93b88
Robin De Schepper <robingilbert.deschepper@unipv.it>
2021-10-06 16:23:15 +0200

    IOError due to growing device label (#292)
    * Prevent ever growing device label and IO-error when looping. Fixes #291
    
    * syntax oopsie
    
    * another big syntax oopsie

--- bsb/simulators/nest.py
+++ bsb/simulators/nest.py
@@ -1063,7 +1063,9 @@ class SpikeDetectorProtocol(DeviceProtocol):
             )
         device_tag = str(_randint())
         device_tag = mpi4py.MPI.COMM_WORLD.bcast(device_tag, root=0)
-        self.device.parameters["label"] += device_tag
+        if not hasattr(self.device, "_orig_label"):
+            self.device._orig_label = self.device.parameters["label"]
+        self.device.parameters["label"] = self.device._orig_label + device_tag
         if mpi4py.MPI.COMM_WORLD.rank == 0:
             self.device.adapter.result.add(SpikeRecorder(self.device))
 
