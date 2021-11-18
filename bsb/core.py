3fcfec24fb641a367fd8f8b99d34d67e7713e5c8
Robin De Schepper <robin.deschepper93@gmail.com>
2021-11-18 09:55:07 +0100

    added section target registration

--- bsb/core.py
+++ bsb/core.py
@@ -1190,3 +1190,9 @@ def register_cell_targetting(name, f):
     from .simulation.targetting import TargetsNeurons
 
     setattr(TargetsNeurons, f"_targets_{name}", f)
+
+
+def register_section_targetting(name, f):
+    from .simulation.targetting import TargetsSections
+
+    setattr(TargetsSections, f"_section_target_{name}", f)
