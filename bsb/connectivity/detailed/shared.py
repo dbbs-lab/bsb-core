60d3cad59b67dd07be4d8ff5348e63160e21f854
Robin De Schepper <robin.deschepper93@gmail.com>
2022-01-10 19:42:48 +0100

    `Intersectional.get_region_of_interest` max rectangle from morpho meta

--- bsb/connectivity/detailed/shared.py
+++ bsb/connectivity/detailed/shared.py
@@ -1,6 +1,18 @@
-from random import choice as random_element
+from itertools import chain
+from functools import reduce
 
 
 class Intersectional:
     def get_region_of_interest(self, chunk, chunk_size):
-        print(self.cell_types)
+        ps_list = [ct.get_placement_set([chunk]) for ct in self.presynaptic.cell_types]
+        ms_list = [ps.load_morphologies() for ps in ps_list]
+        metas = list(chain.from_iterable(ms.iter_meta(unique=True) for ms in ms_list))
+        # TODO: Combine morphology extension information with PS rotation information.
+        _min = reduce(lambda a, b: tuple(map(min, a, b["lsb"])), metas, (0, 0, 0))
+        _max = reduce(lambda a, b: tuple(map(max, a, b["msb"])), metas, (0, 0, 0))
+        print(
+            f"The morphologies extend maximally {_min} towards more negative coordinates"
+        )
+        print(
+            f"The morphologies extend maximally {_max} towards more positive coordinates"
+        )
