409729962df8eb54a389ca59605edc5bb4f8fc93
Robin De Schepper <robin.deschepper93@gmail.com>
2022-01-18 15:14:40 +0100

    Force init using -m?

--- bsb/__main__.py
+++ bsb/__main__.py
@@ -1,3 +1,8 @@
 from . import cli
 
+print("Fragile MPI test")
+from mpi4py import MPI
+
+print("MPI ramk:", MPI.COMM_WORLD.Get_rank())
+print("MPI size:", MPI.COMM_WORLD.Get_size())
 cli.scaffold_cli()
