4daedb96c295bc6f3c4d7a2858e971def352afec
Robin De Schepper <robingilbert.deschepper@unipv.it>
2022-01-19 18:10:49 +0100

    Added number of connection statistics (#338)

--- bsb/statistics.py
+++ bsb/statistics.py
@@ -2,3 +2,7 @@ class Statistics:
     def __init__(self, scaffoldInstance):
         self.scaffold = scaffoldInstance
         self.cells_placed = {k: 0 for k in self.scaffold.configuration.cell_types}
+
+    @property
+    def connections(self):
+        return {cs.tag: len(cs) for cs in self.scaffold.get_connectivity_sets()}
