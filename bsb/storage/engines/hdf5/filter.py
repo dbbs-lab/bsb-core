97f0453a3f951d5cfb05ae9dae49aaaf58869539
Robin De Schepper <robin.deschepper93@gmail.com>
2022-01-25 21:29:22 +0100

    Removed filter "implementation"

--- bsb/storage/engines/hdf5/filter.py
+++
@@ -1,9 +0,0 @@
-from ...interfaces import Filter as IFilter
-from .resource import Resource
-import numpy as np
-
-
-class Filter(IFilter):
-    @classmethod
-    def get_filter_types(cls):
-        return ["label"]
