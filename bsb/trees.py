725e0fcf24613b597eed34c9135877492de3f95d
Robin De Schepper <robin.deschepper93@gmail.com>
2022-01-26 15:42:41 +0100

    Simple BoxTree interface

---
+++ bsb/trees.py
@@ -0,0 +1,22 @@
+from rtree import index as rtree
+
+
+class BoxTreeInterface(abc.ABC):
+    @abc.abstractmethod
+    def query(self, boxes):
+        pass
+
+
+class BoxRTree(BoxTreeInterface):
+    def __init__(self, boxes):
+        self._rtree = rtree.Index(properties=rtree.Property(dimension=3))
+        for id, box in enumerate(boxes):
+            self._rtree.insert(id, box)
+
+    def query(self, boxes):
+        return [[*self._rtree.intersection(box, objects=False)] for box in boxes]
+
+
+# Cheapo provider
+class BoxTree(BoxRTree):
+    pass
