7ff6c7bdecb9cae5f772e44b2f71c2a74670be3d
Robin De Schepper <robin.deschepper93@gmail.com>
2022-01-27 18:31:14 +0100

    Added voxelset API

--- bsb/voxels.py
+++ bsb/voxels.py
@@ -9,43 +9,163 @@ import nrrd
 class VoxelSet:
     def __init__(self, voxels, size, voxel_data=None, irregular=False):
         voxel_size = np.array(size, copy=False)
-        if len(size.shape) > 1:
+        if not len(voxel_size.shape):
+            self._cubic = True
+        if len(voxel_size.shape) > 1:
             # Voxels given in spatial coords with individual size
-            self._sizes = size
+            self._sizes = voxel_size
             self._coords = voxels
+            self._regular = False
         elif irregular:
             # Voxels given in spatial coords but of equal size
-            self._size = size
+            self._size = voxel_size
             self._coords = voxels
+            self._regular = False
         else:
             # Voxels given in index coords
-            self._size = size
+            self._size = voxel_size
             self._indices = voxels
+            self._regular = True
         self._voxel_data = voxel_data
 
+    def __iter__(self):
+        return iter(self.raw(copy=False))
+
+    def __len__(self):
+        return len(self.raw(copy=False))
+
+    def __getitem__(self, index):
+        voxels = self.raw(copy=False)[index]
+        if self.has_equal_sizes:
+            voxel_size = self._size
+        else:
+            voxel_size = self._sizes[index]
+        if self.has_data:
+            voxel_data = self._voxel_data[index]
+        else:
+            voxel_data = None
+        return VoxelSet(voxels, voxel_size, voxel_data)
+
     @property
-    def have_data(self):
+    def has_data(self):
         return self._voxel_data is not None
 
-    def as_spatial_coords(self, copy=True):
-        if hasattr(self, "_coords"):
-            coords = self._coords
-        else:
-            coords = self._to_spatial_coords()
-        return coords.copy() if copy else coords
+    @property
+    def regular(self):
+        return self._regular
 
-    def as_index_coords(self, copy=True):
-        if hasattr(self, "_coords"):
-            coords = self._coords
+    @property
+    def has_equal_sizes(self):
+        # One size fits all
+        return hasattr(self, "_size")
+
+    @property
+    @functools.cache
+    def bounds(self):
+        return (
+            np.min(self.as_spatial_coords(copy=False), axis=0),
+            np.max(self.as_spatial_coords(copy=False), axis=0),
+        )
+
+    @classmethod
+    def concatenate(cls, *sets):
+        if (
+            any(s.has_data for s in sets)
+            or any(not s.regular for s in sets)
+            or any(s._size != sets[0]._size for s in sets)
+        ):
+            raise NotImplementedError(
+                "Can only concat same size index coords without data for now"
+            )
+        alloc = np.empty(
+            (
+                sum(s.raw(copy=False).shape[0] for s in sets),
+                sets[0].raw(copy=False).shape[1],
+            )
+        )
+        ptr = 0
+        for s in sets:
+            l = len(s)
+            alloc[ptr : (ptr + l)] = s.raw(copy=False)
+        return VoxelSet(alloc, sets[0]._size)
+
+    def raw(self, copy=True):
+        coords = self._indices if self.regular else self._coords
+        if copy:
+            coords = coords.copy()
+        return coords
+
+    @property
+    def size(self):
+        if self.has_equal_sizes:
+            return np.array(self._size)
         else:
+            return np.array(self._sizes)
+
+    def get_size_matrix(self, copy=True):
+        if self.has_equal_sizes:
+            size = np.ones(3) * self._size
+            sizes = np.tile(size, len(self.raw(copy=False)))
+        else:
+            sizes = self._sizes
+            if copy:
+                sizes = sizes.copy()
+        return sizes
+
+    def as_spatial_coords(self, copy=True):
+        if self.regular:
             coords = self._to_spatial_coords()
-        return coords.copy() if copy else coords
+        else:
+            coords = self._coords
+            if copy:
+                coords = coords.copy()
+        return coords
 
     def as_boxtree(self, cache=False):
         if cache:
             return self._boxtree_cache()
         else:
             return self._boxtree()
+
+    def snap_to_grid(self, grid_size, unique=False):
+        if self.regular:
+            print(
+                "regular voxels",
+                self._indices,
+                grid_size,
+                self._size,
+                (grid_size / self._size),
+            )
+            grid = self._indices // (grid_size / self._size)
+            print("outcome", grid)
+        else:
+            print("irregular voxels", self._coords, grid_size)
+            grid = self._coords // grid_size
+        voxel_data = self._voxel_data
+        if unique:
+            if self.has_data:
+                grid, id = np.unique(grid, return_index=True, axis=0)
+                voxel_data = voxel_data[id]
+            else:
+                grid = np.unique(grid, axis=0)
+        return VoxelSet(grid, grid_size, voxel_data)
+
+    def select(self, ldc, size):
+        voxel_data = self._voxel_data
+        coords = self.as_spatial_coords(copy=False)
+        print("Looking for", ldc, size, "with voxelset in", self.bounds)
+        inside = np.all(np.logical_and(ldc <= coords, coords < ldc + size), axis=1)
+        print(sum(inside))
+        return self[inside]
+
+    def select_chunk(self, chunk, chunk_size):
+        return self.select(np.array(chunk) * chunk_size, chunk_size)
+
+    def unique(self):
+        raise NotImplementedError("and another one")
+
+    def _to_spatial_coords(self):
+        return self._indices * self._size
 
     @functools.cache
     def _boxtree_cache(self):
@@ -88,6 +208,27 @@ class VoxelSet:
         voxels = np.array(tuple(voxel_reduce.keys()))
         voxel_data = list(voxel_reduce.values())
         return cls(voxels, voxel_size, voxel_data=voxel_data)
+
+
+@config.dynamic(
+    attr_name="type", auto_classmap=True, required=True, type=types.in_classmap()
+)
+class VoxelLoader(abc.ABC):
+    @abc.abstractmethod
+    def get_voxelset(self):
+        pass
+
+
+@config.node
+class NrrdVoxelLoader(VoxelLoader, classmap_entry="nrrd"):
+    source = config.attr(type=str, required=True)
+    mask_value = config.attr(type=int, required=True)
+    voxel_size = config.attr(type=types.voxel_size(), required=True)
+
+    def get_voxelset(self):
+        data, header = nrrd.read(self.source)
+        voxels = np.transpose(np.nonzero(data == self.mask_value))
+        return VoxelSet(voxels, self.voxel_size)
 
 
 def _eq_sides(sides, n):
