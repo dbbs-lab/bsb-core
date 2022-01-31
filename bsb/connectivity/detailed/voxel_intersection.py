b133dc1e1e86f07e7d1de8197852c395473292e9
Robin De Schepper <robin.deschepper93@gmail.com>
2022-01-31 11:43:16 +0100

    Only `_pick_locations` left, and storing the data in CS

--- bsb/connectivity/detailed/voxel_intersection.py
+++ bsb/connectivity/detailed/voxel_intersection.py
@@ -66,6 +66,7 @@ class VoxelIntersection(Intersectional, ConnectionStrategy):
         target_itrs = zip(tset.load_positions(), tset.load_rotations().iter(), tm_iter)
         rotations = cset.load_rotations()
         positions = cset.load_positions()
+        data_acc = []
         for target, candidates in enumerate(matches):
             tpos, trot, tmor = next(target_itrs)
             if not candidates:
@@ -76,7 +77,7 @@ class VoxelIntersection(Intersectional, ConnectionStrategy):
                 voxels = tmor.cached_voxelize(N=self._n_tvoxels)
             else:
                 voxels = tmor.voxelize(N=self._n_tvoxels)
-            tree = voxels.as_boxtree(cache=self.cache)
+            tree = tvoxels.as_boxtree(cache=self.cache)
             print("Target bounds", tmor.bounds)
             for cand in candidates:
                 cpos = positions[cand]
@@ -94,10 +95,12 @@ class VoxelIntersection(Intersectional, ConnectionStrategy):
                 morpho.rotate(crot)
                 morpho.translate(cpos - tpos)
                 morpho.rotate(trot.inv())
-                voxels = morpho.voxelize(N=self._n_cvoxels)
-                overlap = list(tree.query(voxels.as_boxes()))
-                target_voxels = [i for i, v in enumerate(overlap) if v]
-                candidate_voxels = set(itertools.chain.from_iterable(overlap))
+                cvoxels = morpho.voxelize(N=self._n_cvoxels)
+                overlap = list(tree.query(cvoxels.as_boxes()))
+                # Filter out the indices of target voxels that have candidate voxels.
+                target_voxelids = [i for i, v in enumerate(overlap) if v]
+                # Flatten into a set of candidate voxels
+                candidate_voxelids = set(itertools.chain.from_iterable(overlap))
                 print(
                     "Overlap:",
                     len(target_voxels),
@@ -106,4 +109,30 @@ class VoxelIntersection(Intersectional, ConnectionStrategy):
                     len(tree),
                     len(voxels),
                 )
-                # data = voxels.get_data(candidate_voxels)
+                tlocs = tvoxels.get_data(target_voxelids)
+                clocs = coxels.get_data(candidate_voxelids)
+                if self.favor_cache == "pre":
+                    pre_id, post_id = target, cand
+                    pre, post = (tlocs, clocs)
+                    preset, postset = tset, cset
+                else:
+                    pre_id, post_id = cand, target
+                    pre, post = (clocs, tlocs)
+                    preset, postset = cset, tset
+                data_acc.append(self._pick_locations(pre_id, post_id, pre, post))
+
+        # Preallocating and filling is faster than `np.concatenate` :shrugs:
+        acc_idx = np.cumsum(itertools.chain((0,), (len(a[0]) for a in data_acc)))
+        src_locs = np.empty((pairs_found, 3))
+        dest_locs = np.empty((pairs_found, 3))
+        for (s, e), (srcl, destl) in zip(_pairwise(acc_idx), data_acc):
+            src_locs[s:e] = srcl
+            dest_locs[s:e] = destl
+
+        self.connect_cells(preset, postset, src_locs, dest_locs)
+
+
+def _pairwise(iterable):
+    a, b = tee(iterable)
+    next(b, None)
+    return zip(a, b)
