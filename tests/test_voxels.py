ec0d7cdcdabf995d33fea7d52e1d53d4e5cd0a33
Robin De Schepper <robin.deschepper93@gmail.com>
2022-02-03 15:21:26 +0100

    75% :)

--- tests/test_voxels.py
+++ tests/test_voxels.py
@@ -1,6 +1,7 @@
 import unittest
 import numpy as np
 from bsb.voxels import VoxelSet
+from bsb.morphologies import Branch, Morphology
 from itertools import count as _ico, chain as _ic
 
 
@@ -39,6 +40,11 @@ class TestVoxelSet(unittest.TestCase):
             vs([[0, 0, 0], [1, 0, 0], [2, 0, 0]], [0, 0, 0]),
             vs([[0, 0, 0], [1, 0, 0], [2, 0, 0]], [[0, 0, 0], [2, 2, 2], [5, 3, 7]]),
         ]
+        self.dupes = [
+            vs([[0, 0, 0], [0, 0, 0], [0, 0, 0]], 1),
+            vs([[5, 0, 0], [1, 0, 0], [5, 0, 0]], 0),
+            vs([[0, -1, 0], [1, 9, 0], [0, -1, 0]], [[0, 2, 0], [2, 2, 2], [5, 3, 7]]),
+        ]
         self.empty = vs.empty()
         self.all = dict(
             _ic(
@@ -46,9 +52,18 @@ class TestVoxelSet(unittest.TestCase):
                 zip((f"irregular_{i}" for i in _ico()), self.irregulars),
                 zip((f"unequal_{i}" for i in _ico()), self.unequals),
                 zip((f"zero_sized_{i}" for i in _ico()), self.zero_sized),
+                zip((f"dupes_{i}" for i in _ico()), self.dupes),
                 zip((f"empty_{i}" for i in _ico()), (self.empty,)),
             )
         )
+
+        self.data = [
+            vs([[0, 0, 0], [1, 0, 0], [2, 0, 0]], 2, [[1], [0], [1]]),
+            vs([[0, 0, 0], [1, 0, 0], [2, 0, 0]], [2, 2, 2], [[1], [0], [1]]),
+            vs([[0, 0, 0], [1, 0, 0], [2, 2, 0]], [-1, 2, 2], [[1], [0], [1]]),
+            vs([[0, 0, 0], [1, 0, 0], [2, 0, 0]], -1, [[1], [0], [1]]),
+            vs([[0, 0, 0], [1, 0, 0], [0, 0, 0]], -1, [[1], [0], [1]]),
+        ]
 
     def test_weird_usage(self):
         arr = np.array([])
@@ -61,6 +76,23 @@ class TestVoxelSet(unittest.TestCase):
         self.assertTrue(a.is_empty)
         with self.assertRaises(AttributeError):
             a.size = 10
+
+    def test_ctor(self):
+        with self.assertRaises(ValueError):
+            VoxelSet([1, 2, 3], [[1, 0, 0]])
+        with self.assertRaises(ValueError):
+            VoxelSet([[1, 0, 0, 0]], [[1, 0, 0]])
+
+    def test_data_ctor(self):
+        with self.assertRaises(ValueError):
+            VoxelSet([], [], [1])
+        with self.assertRaises(ValueError):
+            VoxelSet([], [], 1)
+        VoxelSet([[1, 2, 3]], [[1, 0, 0]], [1])
+        VoxelSet([[1, 2, 3]], 1, [1])
+        VoxelSet([[1, 2, 3]], 1, [[1, 2]])
+        with self.assertWarns(np.VisibleDeprecationWarning):
+            VoxelSet([[1, 2, 3], [0, 0, 0]], 1, [[1, 2], [1]])
 
     def test_unequal_len(self):
         with self.assertRaises(ValueError):
@@ -131,16 +163,108 @@ class TestVoxelSet(unittest.TestCase):
             with self.subTest(label=label):
                 self.assertEqual(None, set.get_data(), "No index no data should be None")
                 self.assertEqual(None, set.get_data([1, 2]), "No data should be None")
+                self.assertEqual(None, set.data, "No data should be None")
+
+    def test_snap_to_grid(self):
+        for label, set in self.all.items():
+            with self.subTest(label=label):
+                vs = set.snap_to_grid([1, 1, 1])
+                self.assertEqual(len(set), len(vs), "voxels dropped")
+                vs = set.snap_to_grid([1, 1, 0])
+                self.assertTrue(vs.regular, "snap to grid should make regular grid")
+                self.assertClose(0, vs.raw[:, 2], "0 width dimension should be flat")
+        for set in self.data:
+            vs = set.snap_to_grid([1, 1, 1])
+            self.assertEqual(set.data.shape, vs.data.shape, "snap to grid changed data")
+            self.assertClose(set.data, vs.data, "snap to grid changed data")
+
+    def test_snap_unique(self):
+        for set in self.dupes:
+            vs = set.snap_to_grid([1, 1, 1])
+            self.assertEqual(len(set), len(vs), "voxels dropped without unique")
+            vs = set.snap_to_grid([1, 1, 1], unique=True)
+            self.assertNotEqual(len(set), len(vs), "no duplicates dropped with unique")
+        for set in self.data:
+            vs = set.snap_to_grid([1, 1, 1], unique=True)
+            self.assertEqual(len(vs.raw), len(vs.data), "data wrong with unique")
 
     def test_get_size_matrix(self):
         for label, set in self.all.items():
             with self.subTest(label=label):
                 sm = set.get_size_matrix(copy=False)
-                self.assertClose(set.raw(copy=False).shape, sm.shape)
+                self.assertClose(set.get_raw(copy=False).shape, sm.shape)
 
     def test_of_equal_size(self):
         self.assertFalse(self.unequals[2]._single_size, "not internally equal")
         self.assertTrue(self.unequals[2].of_equal_size, "but actually equal")
+
+    def test_raw(self):
+        for label, set in self.all.items():
+            with self.subTest(label=label):
+                # Test that it functions for all types
+                r = set.get_raw(copy=False)
+                self.assertIsNot(r, set.raw, "copy failed")
+
+    def test_iter(self):
+        for label, set in self.all.items():
+            with self.subTest(label=label):
+                y = set.get_raw(copy=False)
+                for i, vox in enumerate(set):
+                    self.assertClose(vox, y[i], "voxel iteration")
+
+    def test_bool(self):
+        self.assertTrue(self.empty.is_empty, "Empty not empty")
+        self.assertFalse(self.empty, "Empty set should not be True")
+        for label, set in self.all.items():
+            if label.startswith("empty"):
+                continue
+            with self.subTest(label=label):
+                self.assertFalse(set.is_empty, "non empty set empty")
+                self.assertTrue(set, "non empty set False")
+
+    def test_from_flat_morphology(self):
+        branches = [
+            Branch([i, i, i, i, i], [0, 1, 2, 3, 4], [0] * 5, [1] * 5) for i in range(5)
+        ]
+        morpho = Morphology(branches)
+        vs = morpho.voxelize(16)
+        self.assertLess(0, len(vs), "Empty voxelset from non empty morpho")
+        self.assertClose(0, vs.get_raw(copy=False)[:, 2], "Flat morphology not flat VS")
+        data = vs.get_data()
+        vs = VoxelSet.from_morphology(morpho, 16, with_data=False)
+        self.assertLess(0, len(vs), "Empty voxelset from non empty morpho")
+        self.assertClose(0, vs.get_raw(copy=False)[:, 2], "Flat morphology not flat VS")
+
+    def test_from_empty_morphology(self):
+        empty_morpho = Morphology([])
+        vs = empty_morpho.voxelize(16)
+        self.assertEqual(0, len(vs), "Non-empty voxelset from empty morpho")
+
+    def test_from_point_morphology(self):
+        point_morpho = Morphology([Branch([100], [1], [100], [1])])
+        vs = point_morpho.voxelize(16)
+        self.assertEqual(1, len(vs), "Point morpho")
+        self.assertClose(0, vs.get_raw(copy=False))
+
+    def test_index(self):
+        pass
+        # for label, set in self.all.items():
+        #     vs = set[0:0, 0, 0, 0]
+        #     print(set[0:0, 0])
+        #     print(len(vs), vs.get_raw(copy=False).shape)
+        #     with self.assertRaises(IndexError):
+        #         vs = set[0:0, 0]
+        #     if not vs.is_empty:
+        #         print(vs)
+        #         print(len(vs), vs.get_raw(copy=False).shape)
+
+    def test_copy(self):
+        for label, set in self.all.items():
+            with self.subTest(label=label):
+                self.assertIsNot(set, set.copy(), "copy failed")
+                self.assertIsNot(
+                    set.get_raw(copy=False), set.copy().get_raw(copy=False), "copy failed"
+                )
 
     def test_concatenate(self):
         ...
