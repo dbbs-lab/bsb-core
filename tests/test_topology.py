f1738fe1da06d888ce4c6fb269f3074121fbf0b4
Robin De Schepper <robin.deschepper93@gmail.com>
2022-02-08 21:49:51 +0100

    fixed tests

--- tests/test_topology.py
+++ tests/test_topology.py
@@ -3,9 +3,8 @@ import unittest, numpy as np
 
 
 def single_layer():
-    r = topology.Stack(partitions=[])
-    c = topology.Layer(thickness=150, stack_index=0, region=r)
-    r._partitions = [c]
+    c = topology.Layer(thickness=150, stack_index=0, region="r")
+    r = topology.Stack(partitions=[c])
     r.arrange(topology.Boundary([0, 0, 0], [100, 100, 100]))
     return r, c
 
@@ -14,10 +13,6 @@ class TestTopology(unittest.TestCase):
     def test_create_topology(self):
         r = topology.Region(partitions=[])
         rb = topology.Region(partitions=[])
-        # Workaround for _partitions only being created when references are resolved on
-        # root config construction
-        r._partitions = []
-        rb._partitions = []
         t = topology.create_topology([r])
         self.assertEqual(r, t, "Topology with 1 root region should be the region itself")
         t = topology.create_topology([r, rb])
@@ -62,10 +57,9 @@ class TestTopology(unittest.TestCase):
         self.assertEqual([50, 50, 50], list(ct.mdc))
 
     def test_ystack(self):
-        r = topology.Stack(partitions=[])
-        c = topology.Layer(thickness=150, stack_index=0, region=r)
-        c2 = topology.Layer(thickness=150, stack_index=1, region=r)
-        r._partitions = [c2, c]
+        c = topology.Layer(thickness=150, stack_index=0, region="r")
+        c2 = topology.Layer(thickness=150, stack_index=1, region="r")
+        r = topology.Stack(partitions=[c, c2])
         r.arrange(topology.Boundary([0, 0, 0], [100, 100, 100]))
         self.assertEqual(0, c.boundaries.y)
         self.assertEqual(150, c2.boundaries.y)
