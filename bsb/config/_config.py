d7f532c1abbc36d8bfc4bc3599f842b9737b1750
Robin De Schepper <robin.deschepper93@gmail.com>
2022-02-22 14:31:25 +0100

    Configs can be adjusted to their current storage

--- bsb/config/_config.py
+++ bsb/config/_config.py
@@ -70,6 +70,12 @@ class Configuration:
             node.scaffold = scaffold
             run_hook(node, "boot")
 
+    def _update_storage_node(self, storage):
+        if self.storage.engine != storage.format:
+            self.storage.engine = storage.format
+        if self.storage.root != storage.root:
+            self.storage.root = storage.root
+
     def __str__(self):
         return str(self.__tree__())
 
