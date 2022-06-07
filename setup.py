2d9a56afa179a6c78e34be2cece6b9af8172b30c
Robin De Schepper <robin.deschepper93@gmail.com>
2022-06-07 14:29:21 +0200

    fix 3.8 register bug by patching functools

--- setup.py
+++ setup.py
@@ -45,8 +45,6 @@ setuptools.setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Operating System :: OS Independent",
