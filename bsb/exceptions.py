836d950e8841bf6e4aef4447dc961dec5f0067d8
Robin De Schepper <robin.deschepper93@gmail.com>
2022-06-25 19:24:56 +0200

    Added SelectorError category

--- bsb/exceptions.py
+++ bsb/exceptions.py
@@ -99,6 +99,7 @@ _t(
             ChunkError=_e(),
             ContinuityError=_e(),
         ),
+        SelectorError=_e(),
         TreeError=_e(),
         VoxelSetError=_e(
             EmptyVoxelSetError=_e(),
