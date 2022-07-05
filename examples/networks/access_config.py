6e2c2f89ef1dbd634b7b66fc476a94bf71270e6f
Robin De Schepper <robin.deschepper93@gmail.com>
2022-07-05 18:48:30 +0200

    meh

--- examples/networks/access_config.py
+++ examples/networks/access_config.py
@@ -3,8 +3,9 @@ from bsb.core import from_hdf5
 
 network = from_hdf5("network.hdf5")
 print("My network was configured with", network.configuration)
-print(
-    # To avoid some needless typing and repetition
+print("My network has", len(network.configuration.cell_types), "cell types")
+(
+    # But to avoid some needless typing and repetition,
     network.cell_types is network.configuration.cell_types
     and network.placement is network.configuration.placement
     and "so on"
