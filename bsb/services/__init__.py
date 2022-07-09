43988c80d258d0f30036144b149437ffac8e2e79
Robin De Schepper <robin.deschepper93@gmail.com>
2022-07-09 15:56:20 +0200

    Updated service module to include new services

--- bsb/services/__init__.py
+++ bsb/services/__init__.py
@@ -1,7 +1,11 @@
-from .mpi import MPIProvider as _MPIProvider
 from ._provider import ErrorProvider as _ErrorProvider
+from .mpi import MPIProvider as _MPIProvider
+from .mpilock import MPILockProvider as _MPILockProvider
 
 MPI = _MPIProvider("mpi4py.MPI").COMM_WORLD
+MPILock = _MPILockProvider("mpilock")
+
+from .pool import JobPool
 
 
 def __getattr__(attr):
