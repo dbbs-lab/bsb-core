45d8e853a6d666967e35056335dad3e7ad0f4bf7
Dario Sannino <105153905+dariosannino@users.noreply.github.com>
2022-09-13 21:31:15 +0200

    Feature/useful str draft (#559)
    * object string insert decorator
    
    * Added CellType str representation
    
    * Implemented a useful __repr__ for Scaffold
    
    * added __repr__ method for class PlacementSet(Interface)
    
    * trial for __repr__ of ConnectivitySet(Interface)
    
    * added __repr__ method for PlacementStrategy and ConnectionStrategy classes
    
    * last correction in PlacementStrategy and ConnectionStrategy classes
    
    * formatted
    
    * added a small test that both `str` and `repr` are not their obj analog
    
    Co-authored-by: Robin De Schepper <robin.deschepper93@gmail.com>

---
+++ tests/test_util.py
@@ -0,0 +1,46 @@
+import os
+import sys
+import unittest
+import numpy as np
+import inspect
+from bsb.core import Scaffold
+from bsb.voxels import VoxelSet
+from bsb.exceptions import *
+import bsb.unittest
+from bsb.unittest import (
+    FixedPosConfigFixture,
+    RandomStorageFixture,
+    NumpyTestCase,
+)
+
+
+class TestNetworkUtil(
+    FixedPosConfigFixture,
+    RandomStorageFixture,
+    NumpyTestCase,
+    unittest.TestCase,
+    engine_name="hdf5",
+):
+    def setUp(self):
+        super().setUp()
+        self.network = Scaffold(self.cfg, self.storage)
+        self.network.connectivity.add(
+            "all_to_all",
+            dict(
+                strategy="bsb.connectivity.AllToAll",
+                presynaptic=dict(cell_types=["test_cell"], subcell_labels=["axon"]),
+                postsynaptic=dict(cell_types=["test_cell"], subcell_labels=["dendrites"]),
+            ),
+        )
+        self.network.compile()
+
+    def test_str(self):
+        for obj in (
+            self.network,
+            *self.network.placement.values(),
+            *self.network.connectivity.values(),
+            self.network.get_placement_set("test_cell"),
+            self.network.get_connectivity_set("test_cell_to_test_cell"),
+        ):
+            self.assertNotEqual(object.__repr__(obj), str(obj))
+            self.assertNotEqual(object.__repr__(obj), repr(obj))
