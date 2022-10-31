1c21eb259a6f9ec21c7cd02a2664900960fa906a
Francesco Sheiban <frances.j.shei@gmail.com>
2022-10-31 10:45:48 +0100

    Updated SWC IO test

--- tests/test_morphologies.py
+++ tests/test_morphologies.py
@@ -1,3 +1,4 @@
+from calendar import c
 import unittest, os, sys, numpy as np, h5py
 import json
 import itertools
@@ -76,11 +77,21 @@ class TestIO(NumpyTestCase, unittest.TestCase):
                     and (swc_data := [float(x) for x in line.split() if x != ""])
                 ]
             )
+        converted_samples = m.to_graph_array()[:, 0].astype(int)
         converted_labels = m.to_graph_array()[:, 1].astype(int)
+        converted_points = m.to_graph_array()[:, 2:5].astype(float)
+        converted_radii = m.to_graph_array()[:, 5].astype(float)
+        converted_parents = m.to_graph_array()[:, 6].astype(int)
+        self.assertTrue(np.array_equal(data[:, 0].astype(int), converted_samples))
         self.assertTrue(np.array_equal(data[:, 1].astype(int), converted_labels))
+        self.assertClose(data[:, 2:5].astype(float), converted_points)
+        self.assertTrue(np.array_equal(data[:, 5].astype(float), converted_radii))
+        self.assertTrue(np.array_equal(data[:, 6].astype(int), converted_parents))
+
         with self.assertRaises(NotImplementedError):
-            file = get_morphology_path("PurkinjeCell.swc")
-            m = Morphology.from_swc(file)
+            b = _branch(10)
+            b.label(["B", "A"], [0, 1, 2])
+            m = Morphology([b])
             m.to_graph_array()
 
 
