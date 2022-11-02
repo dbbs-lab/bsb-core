d5508e74ab12f8d611ad49fc1ec86be381790913
Robin De Schepper <robin.deschepper93@gmail.com>
2022-11-02 13:52:51 +0100

    Import devices module to load classmap

--- bsb/simulators/neuron/__init__.py
+++ bsb/simulators/neuron/__init__.py
@@ -1,6 +1,7 @@
 from bsb.simulation import SimulationBackendPlugin
 from .adapter import NeuronAdapter
 from .simulation import NeuronSimulation
+from . import devices
 
 
 __plugin__ = SimulationBackendPlugin(Simulation=NeuronSimulation, Adapter=NeuronAdapter)
