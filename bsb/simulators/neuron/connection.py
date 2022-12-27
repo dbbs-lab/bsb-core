328faf00129c403a43c52399b7db3b2458ffb90f
Robin De Schepper <robin.deschepper93@gmail.com>
2022-12-27 14:56:22 +0100

    wip connections

--- bsb/simulators/neuron/connection.py
+++ bsb/simulators/neuron/connection.py
@@ -1,37 +1,32 @@
 from bsb import config
 from bsb.config import types
 from bsb.simulation.connection import ConnectionModel
+from bsb.simulation.parameter import Parameter
 
 
-_str_list = types.list(type=str)
+@config.dynamic(
+    attr_name="model_strategy", required=False, default="transceiver", auto_classmap=True
+)
+class NeuronConnection(ConnectionModel):
+    pass
 
 
 @config.node
-class NeuronConnection(ConnectionModel):
-    synapses = config.attr(
-        type=types.or_(types.dict(type=_str_list), _str_list), required=True
+class SynapseSpec:
+    synapse = config.attr(type=str, required=True)
+    parameters = config.list(type=Parameter)
+
+    def __init__(self, synapse_name=None, /, **kwargs):
+        if synapse_name is not None:
+            self._synapse = synapse_name
+
+
+@config.node
+class TransceiverModel(NeuronConnection, classmap_entry="transceiver"):
+    synapses = config.list(
+        type=SynapseSpec,
+        required=True,
     )
-    source = config.attr(type=str, default=None)
 
-    def validate_prepare(self):
-        for connection_model in self.connection_models.values():
-            # Get the connectivity set associated with this connection model
-            connectivity_set = self.scaffold.get_connectivity_set(connection_model.name)
-            from_type = connectivity_set.connection_types[0].presynaptic.type
-            to_type = connectivity_set.connection_types[0].postsynaptic.type
-            from_cell_model = self.cell_models[from_type.name]
-            to_cell_model = self.cell_models[to_type.name]
-            if (
-                from_type.entity
-                or from_cell_model.relay
-                or to_type.entity
-                or to_cell_model.relay
-            ):
-                continue
-            if not connectivity_set.compartment_set.exists():
-                raise IntersectionDataNotFoundError(
-                    "No intersection data found for '{}'".format(connection_model.name)
-                )
-
-    def resolve_synapses(self):
-        return self.synapses
+    def create_connections(self):
+        pass
