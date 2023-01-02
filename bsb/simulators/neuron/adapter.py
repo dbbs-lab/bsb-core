20eace0f36ff59fe0fd4f9b32772fc1161187735
Robin De Schepper <robin.deschepper93@gmail.com>
2023-01-02 14:18:54 +0100

    added connections

--- bsb/simulators/neuron/adapter.py
+++ bsb/simulators/neuron/adapter.py
@@ -2,8 +2,8 @@ import contextlib
 import itertools
 import os
 import time
+from functools import cache
 
-import errr
 import numpy as np
 import typing
 
@@ -22,6 +22,9 @@ class SimulationData:
     def __init__(self):
         self.chunks = None
         self.cells = dict()
+        self.cid_offsets = dict()
+        self.connections = dict()
+        self.first_gid: int = None
         self.result: SimulationResult = None
 
 
@@ -45,6 +48,7 @@ class NeuronAdapter(SimulatorAdapter):
         self.network = None
         self.result = None
         self.simdata = dict()
+        self.next_gid = 0
 
     def prepare(self, simulation, comm=None):
         if self.engine is None:
@@ -58,15 +62,15 @@ class NeuronAdapter(SimulatorAdapter):
             engine.dt = simulation.resolution
             engine.celsius = simulation.temperature
             engine.tstop = simulation.duration
+            simdata = self.simdata[simulation]
 
             report("Load balancing", level=2)
             self.load_balance(simulation)
-            self.simdata[simulation].result = SimulationResult(simulation)
+            simdata.result = SimulationResult(simulation)
             report("Load balancing", level=2)
             self.create_neurons(simulation)
-            MPI.barrier()
             report("Creating transmitters", level=2)
-            self.create_transmitters(simulation)
+            self.create_connections(simulation)
             report("", level=3)
             # self.create_source_vars()
             report("Indexing relays", level=2)
@@ -87,11 +91,16 @@ class NeuronAdapter(SimulatorAdapter):
         return self.simdata[simulation]
 
     def load_balance(self, simulation):
+        simdata = self.simdata[simulation]
         chunk_stats = simulation.scaffold.storage.get_chunk_stats()
         size = MPI.get_size()
-        rank = MPI.get_rank()
         all_chunks = [Chunk.from_id(int(chunk), None) for chunk in chunk_stats.keys()]
-        self.simdata[simulation].chunks = all_chunks[rank::size]
+        simdata.node_chunk_alloc = [all_chunks[rank::size] for rank in range(0, size)]
+        simdata.chunk_node_map = {}
+        for node, chunks in enumerate(simdata.node_chunk_alloc):
+            for chunk in chunks:
+                simdata.chunk_node_map[chunk] = node
+        simdata.chunks = simdata.node_chunk_alloc[MPI.get_rank()]
 
     def run(self, simulation: "Simulation"):
         if simulation not in self.simdata:
@@ -122,130 +131,66 @@ class NeuronAdapter(SimulatorAdapter):
         data.result.flush()
         return data.result
 
-    def create_transmitters(self, simulation):
+    def create_connections(self, simulation):
+        # Offset based deterministic connection to GID algorithm.
         simdata = self.simdata[simulation]
-        # Concatenates all the `from` locations of all intersections together and creates
-        # a network wide map of "signal origins" to NEURON parallel spike GIDs.
+        self._allocate_transmitters(simulation)
+        for conn_model in simulation.connection_models.values():
+            cs = simulation.scaffold.get_connectivity_set(conn_model.name)
+            with fill_parameter_data(conn_model.parameters, []):
+                conn_model.create_connections(simulation, simdata, cs)
 
-        # Fetch all of the connectivity sets that can be transmitters (excludes relays)
-        sets = self._collect_transmitter_sets(simulation)
-        # Get the total size of all intersections
-        total = sum(len(s) for s in sets)
-        # Allocate an array for them
-        alloc = np.empty((total, 2), dtype=int)
-        ptr = 0
-        for connectivity_set in sets:
-            # Get the connectivity set's intersection and slice them into the array.
-            inter = connectivity_set.intersections
-            if not len(inter):
-                continue
-            alloc[ptr : (ptr + len(inter))] = [
-                (i.from_id, i.from_compartment.section_id) for i in inter
-            ]
-            # Move up the pointer for the next slice.
-            ptr += len(inter)
-        unique_transmitters = np.unique(alloc, axis=0)
-        self.transmitter_map = dict(zip(map(tuple, unique_transmitters), range(total)))
-        tcount = 0
-        try:
-            for (cell_id, section_id), gid in self.transmitter_map.items():
-                if cell_id in self.node_cells:
-                    cell = self.cells[cell_id]
-                    cell.create_transmitter(cell.sections[section_id], gid)
-                    tcount += 1
-        except Exception as e:
-            errr.wrap(TransmitterError, e, prepend=f"[{cell_id}] ")
-
+    @cache
+    def _allocate_transmitters(self, simulation):
+        simdata = self.simdata[simulation]
+        first = self.next_gid
+        chunk_stats = simulation.scaffold.storage.get_chunk_stats()
+        max_trans = sum(stats["connections"]["out"] for stats in chunk_stats.values())
         report(
-            f"Node {self.get_rank()} created {tcount} transmitters",
+            f"Node {MPI.get_rank()} allocated GIDs {self.next_gid} to {max_trans}",
             level=3,
             all_nodes=True,
         )
+        simdata.alloc = (first, self.next_gid)
+        simdata.transmap = self._map_transmitters(simulation, simdata)
 
-    def create_source_vars(self):
-        for connection_model in self.connection_models.values():
-            if not connection_model.source:
-                continue
-            source = connection_model.source
-            set = self._model_to_set(connection_model)
-            for inter in set.intersections:
-                cell_id = inter.from_id
-                if cell_id not in self.node_cells:
-                    continue
-                cell = self.cells[cell_id]
-                section_id = inter.from_compartment.section_id
-                section = cell.sections[section_id]
-                gid = self.transmitter_map[(cell_id, section_id)]
-                cell.create_transmitter(cell.sections[section_id], gid, source)
-
-    def _collect_transmitter_sets(self, simulation):
-        models = simulation.connection_models.values()
-        sets = [simulation.scaffold.get_connectivity_set(model.name) for model in models]
-        return [s for s in sets if self._is_transmitter_set(s)]
-
-    def _is_transmitter_set(self, set):
-        print("IS TRANSMCHECK", dir(set))
-        if set.is_orphan():
-            return False
-        name = set.name
-        from_cell_model = self.cell_models[name]
-        return not from_cell_model.relay
-
-    def create_receivers(self):
-        for connection_model in self.connection_models.values():
-            # Get the connectivity set associated with this connection model
-            connectivity_set = self.scaffold.get_connectivity_set(connection_model.name)
-            from_cell_type = connectivity_set.connection_types[0].presynaptic.type
-            if self.cell_models[from_cell_type.name].relay:
-                continue
-            from_cell_model = self.cell_models[from_cell_type.name]
-            to_cell_type = connectivity_set.connection_types[0].postsynaptic.type
-            to_cell_model = self.cell_models[to_cell_type.name]
-            if self.cell_models[to_cell_type.name].relay:
-                raise NotImplementedError("Sorry, no relays yet, only for devices")
-                # Fetch cell and section from `self.relay_scheme`
-                # .get_locations() should offer some insights
-            else:
-                synapse_types = connection_model.resolve_synapses()
-                for intersection in connectivity_set.intersections:
-                    if intersection.to_id in self.node_cells:
-                        cell = self.cells[int(intersection.to_id)]
-                        section_id = int(intersection.to_compartment.section_id)
-                        section = cell.sections[section_id]
-                        gid = self.transmitter_map[
-                            (
-                                intersection.from_id,
-                                intersection.from_compartment.section_id,
-                            )
-                        ]
-                        for synapse_type in synapse_types:
-                            try:
-                                cell.create_receiver(section, gid, synapse_type)
-                            except Exception as e:
-                                raise ScaffoldError(
-                                    "[" + connection_model.name + "] " + str(e)
-                                ) from None
+    @cache
+    def _map_transmitters(self, simulation, simdata):
+        blocks = []
+        for cm, cs in simulation.get_connectivity_sets().items():
+            pre, _ = cs.load_connections().as_globals().all()
+            pre[:, 0] += simdata.cid_offsets[cs.pre_type]
+            blocks.append(pre[:, :2])
+        return {
+            tuple(loc): gid + simdata.alloc[0]
+            for gid, loc in zip(
+                itertools.count(), np.unique(np.concatenate(blocks), axis=0)
+            )
+        }
 
     def create_neurons(self, simulation):
         simdata = self.simdata[simulation]
-        for cell_model in simulation.cell_models.values():
-            if cell_model.relay:
-                continue
+        offset = 0
+        for cell_model in sorted(simulation.cell_models.values()):
             ps = cell_model.cell_type.get_placement_set()
-            for chunk in simdata.chunks:
-                self._create_chunk_neurons(chunk, simdata, simulation, cell_model, ps)
+            simdata.cid_offsets[cell_model.cell_type] = offset
+            with ps.chunk_context(simdata.chunks):
+                self._create_population(simdata, cell_model, ps, offset)
+            offset += len(ps)
 
-    def _create_chunk_neurons(self, chunk, simdata, simulation, cell_model, ps):
-        with ps.chunk_context(chunk):
-            data = []
-            for var in ("positions", "morphologies", "rotations", "additional"):
-                try:
-                    data.append(getattr(ps, f"load_{var}")())
-                except DatasetNotFoundError:
-                    data.append(itertools.repeat(None))
-
-            with fill_parameter_data(cell_model.parameters, data):
-                simdata.cells[chunk] = cell_model.create_instances(len(ps), *data)
+    def _create_population(self, simdata, cell_model, ps, offset):
+        data = []
+        for var in ("positions", "morphologies", "rotations", "additional"):
+            try:
+                data.append(getattr(ps, f"load_{var}")())
+            except DatasetNotFoundError:
+                data.append(itertools.repeat(None))
+        with fill_parameter_data(cell_model.parameters, data):
+            instances = cell_model.create_instances(len(ps), *data)
+            for id, instance in zip(ps.load_ids(), instances):
+                gid = offset + id
+                instance.id = gid
+                simdata.cells[gid] = instance
 
     def prepare_devices(self):
         device_module = __import__("devices", globals(), level=1)
--- bsb/simulators/neuron/connection.py
+++ bsb/simulators/neuron/connection.py
@@ -1,5 +1,9 @@
+import itertools
+from functools import cache
+
+import numpy as np
+
 from bsb import config
-from bsb.config import types
 from bsb.simulation.connection import ConnectionModel
 from bsb.simulation.parameter import Parameter
 
@@ -8,7 +12,10 @@ from bsb.simulation.parameter import Parameter
     attr_name="model_strategy", required=False, default="transceiver", auto_classmap=True
 )
 class NeuronConnection(ConnectionModel):
-    pass
+    def create_connections(self, simulation, simdata, connections, gids):
+        raise NotImplementedError(
+            "Cell models should implement the `create_connections` method."
+        )
 
 
 @config.node
@@ -27,6 +34,27 @@ class TransceiverModel(NeuronConnection, classmap_entry="transceiver"):
         type=SynapseSpec,
         required=True,
     )
+    parameters = config.list(type=Parameter)
+    source = config.attr(type=str)
 
-    def create_connections(self):
-        pass
+    def create_connections(self, simulation, simdata, cs):
+        self.create_transmitters(simdata, cs)
+        self.create_receivers(simdata, cs)
+
+    def create_transmitters(self, simdata, cs):
+        pre, _ = cs.load_connections().from_(simdata.chunks).as_globals().all()
+        pre[:, 0] += simdata.cid_offsets[cs.pre_type]
+        locs = np.unique(pre[:, :2], axis=0)
+        for loc in locs:
+            gid = simdata.transmap[tuple(loc)]
+            simdata.cells[loc[0]].insert_transmitter(gid, (loc[1], 0), source=self.source)
+
+    def create_receivers(self, simdata, cs):
+        pre, post = cs.load_connections().incoming().to(simdata.chunks).as_globals().all()
+        pre[:, 0] += simdata.cid_offsets[cs.pre_type]
+        post[:, 0] += simdata.cid_offsets[cs.post_type]
+        for pre_loc, post_loc in zip(pre[:, :2], post):
+            gid = simdata.transmap[tuple(pre_loc)]
+            cell = simdata.cells[post_loc[0]]
+            for spec in self.synapses:
+                cell.insert_receiver(gid, spec.synapse, post_loc[1:], source=self.source)
