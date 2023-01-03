2d25d156c89bf0ebc5d81d78bd3efee23d3fe1fb
Robin De Schepper <robin.deschepper93@gmail.com>
2023-01-03 14:29:11 +0100

    adapted neuron devices to new dynamic class structure

--- bsb/simulators/neuron/device.py
+++ bsb/simulators/neuron/device.py
@@ -1,8 +1,6 @@
 from bsb import config
-from bsb.exceptions import DeviceConnectionError
-from bsb.config import types
 from bsb.simulation.device import DeviceModel
-from bsb.simulation.targetting import CellTargetting
+from bsb.simulation.targetting import Targetting
 
 
 class TargetLocation:
@@ -15,52 +13,13 @@ class TargetLocation:
         return self.connection and self.connection.synapses
 
 
-@config.dynamic(attr_name="device", type=types.in_classmap(), auto_classmap=True)
+@config.dynamic(attr_name="device", auto_classmap=True)
 class NeuronDevice(DeviceModel):
-    radius = config.attr(type=float)
-    origin = config.attr(type=types.list(type=float, size=3))
-    targetting = config.attr(type=CellTargetting, required=True)
-    io = config.attr(type=types.in_(["input", "output"]), required=True)
+    targetting = config.attr(type=Targetting, required=True)
 
-    def create_patterns(self):
-        raise NotImplementedError(
-            "The "
-            + self.__class__.__name__
-            + " device does not implement any `create_patterns` function."
-        )
-
-    def get_pattern(self, target, cell=None, section=None, synapse=None):
-        raise NotImplementedError(
-            "The "
-            + self.__class__.__name__
-            + " device does not implement any `get_pattern` function."
-        )
-
-    def implement(self, target, location):
+    def implement(self, simulation, simdata):
         raise NotImplementedError(
             "The "
             + self.__class__.__name__
             + " device does not implement any `implement` function."
         )
-
-    def get_locations(self, target):
-        locations = []
-        if target in self.adapter.relay_scheme:
-            for cell_id, section_id, connection in self.adapter.relay_scheme[target]:
-                if cell_id not in self.adapter.node_cells:
-                    continue
-                cell = self.adapter.cells[cell_id]
-                section = cell.sections[section_id]
-                locations.append(TargetLocation(cell, section, connection))
-        elif target in self.adapter.node_cells:
-            try:
-                cell = self.adapter.cells[target]
-            except KeyError:
-                raise DeviceConnectionError(
-                    "Missing cell {} on node {} while trying to implement device '{}'. This can occur if the cell was placed in the network but not represented with a model in the simulation config.".format(
-                        target, self.adapter.get_rank(), self.name
-                    )
-                )
-            sections = self.target_section(cell)
-            locations.extend(TargetLocation(cell, section) for section in sections)
-        return locations
