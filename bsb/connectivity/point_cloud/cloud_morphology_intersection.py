Fixed rotation method: Now it performs a rotation around an axis passing through the point self.center. Cleaned the code a bit

bsb/connectivity/point_cloud/cloud_morphology_intersection.py
@@ -20,23 +20,6 @@ class CloudToMorphologyIntersection(ConnectionStrategy):
 
         ct = self.postsynaptic.cell_types[0]
         chunks = ct.get_placement_set().get_all_chunks()
-        
-
-        """cloud = ShapesComposition(voxel_size)
-        cloud.load_from_file(cloud_name)  
-        mbb_min, mbb_max = cloud.find_mbb()
-        
-        selected_chunks = []
-
-        # Look for chunks inside the mbb
-        #inside = (c[:,0]>mbb_min[0]) & (c[:,1]>mbb_min[1]) & (c[:,2]>mbb_min[2]) & (c[:,0]<mbb_max[0]) & (c[:,1]<mbb_max[1]) & (c[:,2]<mbb_max[2])
-        for c in chunks:    
-            #inside = (c[0]>mbb_min[0]) & (c[1]>mbb_min[1]) & (c[2]>mbb_min[2]) & (c[0]<mbb_max[0]) & (c[1]<mbb_max[1]) & (c[2]<mbb_max[2])
-            inside = (c[0]>mbb_min[0]-c.dimensions[0]) & (c[1]>mbb_min[1]-c.dimensions[1]) & (c[2]>mbb_min[2]-c.dimensions[2]) & (c[0]<mbb_max[0]+c.dimensions[0]) & (c[1]<mbb_max[1]+c.dimensions[1]) & (c[2]<mbb_max[2]+c.dimensions[2])
-            if (inside == True):
-                selected_chunks.append(Chunk([c[0], c[1], c[2]], chunk.dimensions))
-        """
-
         return chunks
 
     def connect(self, pre, post):
@@ -59,12 +42,6 @@ class CloudToMorphologyIntersection(ConnectionStrategy):
             cloud_cache.append(cloud)
         
         cloud_choice_id =  np.random.randint(low=0, high=len(cloud_cache), size=len(pre_pos), dtype=int)
-
-        #
-
-        """cloud = ShapesComposition()
-        cloud.load_from_file(self.presynaptic.cloud_name)
-        cloud = cloud.filter_by_labels(self.presynaptic.morphology_labels)"""
 
         to_connect_pre = np.empty([1,3],dtype=int)
         to_connect_post = np.empty([1,3],dtype=int)
@@ -95,35 +72,23 @@ class CloudToMorphologyIntersection(ConnectionStrategy):
                 post_morpho_coord[local_ptr:local_ptr+len(b.points),:]  = tmp
                 local_ptr += len(b.points)
             
-            """#Find pre minimal bounding box of the morpho
-            post_mbb_min = np.min(post_morpho_coord,axis=0)
-            post_mbb_max = np.max(post_morpho_coord,axis=0)"""
-
             for pre_id, pre_coord in enumerate(pre_pos):
                 
                 pre_cloud = cloud_cache[cloud_choice_id[pre_id]].copy()
                 pre_coord[[1, 2]] = pre_coord[[2, 1]]
                 pre_cloud.translate(pre_coord)
-
                 local_selection = np.empty([morpho_points,3])
 
                 mbb_check = pre_cloud.inside_mbox(post_morpho_coord)
-                #print(mbb_check)
-                #print("---------")
+
                 if np.any(mbb_check):
-                    #print("Found (mbb)")
-                    #print(post_coord)
-                    #print(post_morpho_coord[mbb_check])
                     inside_pts = pre_cloud.inside_shapes(post_morpho_coord[mbb_check])
                     #Find the morpho points inside the cloud
                     if np.any(inside_pts):
-                        #print("Found (shapes)")
                         local_selection = (post_points_ids[mbb_check])[inside_pts]
                         if self.affinity < 1 and len(local_selection) > 0:
                             local_selection = local_selection[np.random.choice(local_selection.shape[0], np.max([1, int(np.floor(self.affinity * len(local_selection)))])),:]
-                        #print(local_selection)
-                        #local_selection = pre_points_ids[inside_pts]
-                        
+
                         selected_count = len(local_selection)
                         if (selected_count > 0):
                             to_connect_post = np.vstack([to_connect_post,local_selection])
@@ -131,5 +96,5 @@ class CloudToMorphologyIntersection(ConnectionStrategy):
                             pre_tmp[:,0] = pre_id
                             to_connect_pre= np.vstack([to_connect_pre,pre_tmp])
 
-        print("Connected", len(pre_pos), "pre cells to",len(post_pos),"post cells.")
+        #print("Connected", len(pre_pos), "pre cells to",len(post_pos),"post cells.")
         self.connect_cells(pre_ps, post_ps, to_connect_pre[1:], to_connect_post[1:])

bsb/connectivity/point_cloud/geometric_shapes.py
@@ -61,17 +61,12 @@ class ShapesComposition:
         self.labels.append(labels)
 
     def filter_by_labels(self, labels : list[str]):
-        #print("filter")
         result = copy.deepcopy(self)
         selected_id = []
         for i,lb_list in enumerate(self.labels):
-            #print(lb_list)
             for to_select in labels: 
-                #print(to_select)
                 if (to_select in lb_list):
-                    #print("YES",to_select)
                     selected_id.append(i)
-        #print(not_selected_id)
         selected_id = set(selected_id)
         not_selected_id = []
         for i in range(len(self.shapes)):
@@ -83,20 +78,6 @@ class ShapesComposition:
         for nn,i in enumerate(selected_id):
             result.shapes.append(copy.deepcopy(self.shapes[i]))
             result.labels.append(copy.deepcopy(self.labels[i]))
-
-        """for i in sorted(not_selected_id,reverse=True):
-            #print(i,"/",len(result.shapes))
-            del result.shapes[i]"""
-        """for nn,i in enumerate(selected_id):
-            print(i)
-            print(self.shapes[i])
-            result.add_shape(copy.deepcopy(self.shapes[i]),copy.deepcopy(self.labels[i]))
-            print(self.shapes[i].mbb_max)
-            print(result.shapes[nn].mbb_max)
-            print(self.shapes[i].mbb_min)
-            print(result.shapes[nn].mbb_min)
-            print("------------------------")"""
-    
         result.mbb_min, result.mbb_max = result.find_mbb()
         return result
 
@@ -121,9 +102,7 @@ class ShapesComposition:
 
         mins = np.empty([len(self.shapes),3])
         maxs = np.empty([len(self.shapes),3])
-        #print(len(self.shapes))
         for i,shape in enumerate(self.shapes):
-            #min, max = shape.find_mbb()
             mins[i,:] = shape.mbb_min
             maxs[i,:] = shape.mbb_max
 
@@ -175,36 +154,14 @@ class ShapesComposition:
             & (points[:, 2] > self.mbb_min[2])
             & (points[:, 2] < self.mbb_max[2])
         )
-        """print("SC inside_mbox")
-        print(points[0])
-        print(self.mbb_min)
-        print(self.mbb_max)
-        print("-------")"""
-
         return inside
 
-        
-        """if len(self.shapes) != 0:
-            cloud = np.full(len(points), 0, dtype=bool)
-            for shape in self.shapes:
-                cloud = cloud | shape.check_mbox(points)
-
-            return cloud
-        else:
-            return None"""
-
     def inside_shapes(self, points):
-        #print("INSIDE_SHAPES")
         if len(self.shapes) != 0:
             cloud = np.full(len(points), 0, dtype=bool)
             for shape in self.shapes:
                 tmp = shape.check_mbox(points)
-                #print(type(shape))
-                #print("pts",points[0])
-                #print("shape min",shape.mbb_min)
-                #print("shape max",shape.mbb_max)
                 if np.any(tmp):
-                    #print("H")
                     cloud = cloud | shape.check_inside(points)
             return cloud
         else:
@@ -429,7 +386,7 @@ class Cone(GeometricShape):
 
     def rotate(self, r_versor: np.ndarray, angle: float):
         rot = R.from_rotvec(r_versor * angle)
-        self.center = rot.apply(self.center)
+        #self.center = rot.apply(self.center)
         self.apex = rot.apply(self.apex)
 
     def generate_point_cloud(self, npoints: int):
@@ -465,27 +422,6 @@ class Cone(GeometricShape):
 
     def check_mbox(self, points: np.ndarray):
 
-        """# Center the points to the origin
-        pts = points - self.apex
-
-        # Vectors identifying half of the sides of the square in xzy
-        u = np.array([self.radius, 0, 0])
-        v = np.array([0, self.radius, 0])
-
-        # Find the rotation angle and axis
-        hv = self.center - self.apex
-        hv = hv / np.linalg.norm(hv)
-        zvers = np.array([0, 0, 1])
-        perp = np.cross(zvers, hv)
-        angle = np.arccos(np.dot(hv, zvers))
-        rot = R.from_rotvec(perp * angle)"""
-
-        """inside = (
-            (points[:, 2] > self.mbb_min[2])
-            & (points[:, 2] < self.mbb_max[2]))
-        if np.any(inside):
-            inside = inside & (points[:, 1] > self.mbb_min[1]) & (points[:, 1] < self.mbb_max[1]) & (points[:, 0] > self.mbb_min[0]) & (points[:, 0] < self.mbb_max[0])"""
-        
         #Look for points inside the mbb.
         inside = (
             (points[:, 0] > self.mbb_min[0])
@@ -596,15 +532,8 @@ class Cylinder(GeometricShape):
         for i,pt in enumerate(extrema):
             extrema[i] = rot.apply(pt)
         
-        #minima = rot.apply(minima) + self.center
-        #maxima = rot.apply(maxima) + self.center
-        #print(minima)
-        #print(maxima)
         maxima = np.max(extrema,axis=0) + self.center
         minima = np.min(extrema,axis=0) + self.center
-        #print("FIND MBB")
-        #print(minima)
-        #print(maxima)
         return minima, maxima
 
     def get_volume(self):
@@ -620,7 +549,7 @@ class Cylinder(GeometricShape):
 
     def rotate(self, r_versor: np.ndarray, angle: float):
         rot = R.from_rotvec(r_versor * angle)
-        self.center = rot.apply(self.center)
+        #self.center = rot.apply(self.center)
         self.height_vector = rot.apply(self.height_vector)
 
     def generate_point_cloud(self, npoints: int):
@@ -649,17 +578,6 @@ class Cylinder(GeometricShape):
 
     def check_mbox(self, points: np.ndarray):
 
-        """print("INSIDE_MBOX_CYLINDER")
-        print(self.mbb_min)
-        print(self.mbb_max)
-        print("cpt", points[0] )"""
-        # Check for intersections with mbb
-        """inside = (
-            (points[:, 2] > self.mbb_min[2])
-            & (points[:, 2] < self.mbb_max[2]))
-        if np.any(inside):
-            inside = inside & (points[:, 1] > self.mbb_min[1]) & (points[:, 1] < self.mbb_max[1]) & (points[:, 0] > self.mbb_min[0]) & (points[:, 0] < self.mbb_max[0])"""
-                 
         inside = (
             (points[:, 0] > self.mbb_min[0])
             & (points[:, 0] < self.mbb_max[0])
@@ -668,8 +586,6 @@ class Cylinder(GeometricShape):
             & (points[:, 2] > self.mbb_min[2])
             & (points[:, 2] < self.mbb_max[2])
         )
-        """print(inside)
-        print("FINE INSIDE_MBOX_CYLINDER")"""
         return inside
 
     def check_inside(self, points: np.ndarray):

bsb/connectivity/point_cloud/morphology_cloud_intersection.py
@@ -61,19 +61,6 @@ class MorphologyToCloudIntersection(ConnectionStrategy):
         
         cloud_choice_id =  np.random.randint(low=0, high=len(cloud_cache), size=len(post_pos), dtype=int)
 
-        #
-
-        """cloud = ShapesComposition()
-        cloud.load_from_file(self.postsynaptic.cloud_name)
-        cloud = cloud.filter_by_labels(self.postsynaptic.morphology_labels)"""
-
-        #print(len(cloud.s))
-
-        #print(self.postsynaptic.morphology_labels)
-        #print("Numero shapes",len(cloud.shapes))
-        #print(dir(cloud.shapes[0]))
-        #print(dir(cloud.shapes[0].radius))
-
         to_connect_pre = np.empty([1, 3], dtype=int)
         to_connect_post = np.empty([1, 3], dtype=int)
 
@@ -82,7 +69,6 @@ class MorphologyToCloudIntersection(ConnectionStrategy):
 
         for pre_id, pre_coord, morpho in zip(itertools.count(), pre_pos, pre_morphos):
             
-            #print(pre_id, "/", len(pre_pos))
             # Get the branches
             branches = morpho.get_branches()
             first_axon_branch_id = branches.index(branches[0])
@@ -104,35 +90,19 @@ class MorphologyToCloudIntersection(ConnectionStrategy):
                 pre_morpho_coord[local_ptr : local_ptr + len(b.points)] = tmp
                 local_ptr += len(b.points)
 
-            """#Find pre minimal bounding box of the morpho
-            pre_mbb_min = np.min(pre_morpho_coord,axis=0)
-            pre_mbb_max = np.max(pre_morpho_coord,axis=0)"""
-
             for post_id, post_coord in enumerate(post_pos):
 
                 post_cloud = cloud_cache[cloud_choice_id[post_id]].copy()
-                #print(post_id, "/", len(post_pos))
-                #print(type(post_coord))
                 #Swap y and z
                 post_coord[[1, 2]] = post_coord[[2, 1]]
                 post_cloud.translate(post_coord)
-                #print(pre_morpho_coord)
                 mbb_check = post_cloud.inside_mbox(pre_morpho_coord)
-                #print(mbb_check)
-                #print("---------")
                 if np.any(mbb_check):
-                    #print("MBB")
-                    #print("Found")
-                    # Find the morpho points inside the cloud
                     inside_pts = post_cloud.inside_shapes(pre_morpho_coord[mbb_check])
-                    #print(np.count_nonzero(inside_pts))
                     if np.any(inside_pts):
-                        #print("Found")
                         local_selection = (pre_points_ids[mbb_check])[inside_pts]
                         if self.affinity < 1 and len(local_selection) > 0:
                             local_selection = local_selection[np.random.choice(local_selection.shape[0], np.max([1, int(np.floor(self.affinity * len(local_selection)))])),:]
-                        #print(local_selection)
-                        #local_selection = pre_points_ids[inside_pts]
 
                         selected_count = len(local_selection)
                         if selected_count > 0:
@@ -141,5 +111,5 @@ class MorphologyToCloudIntersection(ConnectionStrategy):
                             post_tmp[:, 0] = post_id
                             to_connect_post = np.vstack([to_connect_post, post_tmp])
 
-        print("Connected", len(pre_pos), "pre cells to", len(post_pos), "post cells.")
+        #print("Connected", len(pre_pos), "pre cells to", len(post_pos), "post cells.")
         self.connect_cells(pre_ps, post_ps, to_connect_pre[1:], to_connect_post[1:])