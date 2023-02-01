Fix Rotation distributor: use rotation matrix instead of quaternions

bsb/placement/distributor.py
@@ -1,4 +1,5 @@
 from .. import config
+from .._util import rotation_matrix_from_vectors
 from ..config.types import ndarray
 from ..storage import NrrdDependencyNode
 from ..topology.partition import Partition
@@ -218,28 +219,21 @@ class VolumetricRotations(RotationDistributor, classmap_entry="orientation_field
             * (voxel_pos[:, 2] < orientation_field.shape[3])
         )
 
-        orientations = np.zeros((positions.shape[0], 3), dtype=float)
+        orientations = np.full((positions.shape[0], 3), self.default_vector, dtype=float)
         orientations[filter_inside] = np.moveaxis(orientation_field, 0, -1)[
             voxel_pos[filter_inside, 0],
             voxel_pos[filter_inside, 1],
             voxel_pos[filter_inside, 2],
         ]
-        # convert to quaternions
-        t = np.copy(orientations)
-        t[np.isnan(t).any(axis=1)] = self.default_vector  # default_vector -> no rotation
-        w = np.matmul(self.default_vector, np.array(t).T) + np.linalg.norm(
-            self.default_vector, axis=-1
-        ) * np.linalg.norm(t, axis=-1)
+        orientations[
+            np.isnan(orientations).any(axis=1) + ~orientations.any(axis=1)
+        ] = self.default_vector
 
         return RotationSet(
-            np.array(
-                [
-                    Rotation.from_quat(v).as_euler("xyz", degrees=True)
-                    for v in np.hstack(
-                        [w[:, np.newaxis], np.cross(self.default_vector, t)]
-                    )
-                ]
-            )
+            Rotation.from_matrix(
+                rotation_matrix_from_vectors(self.default_vector, v)
+            ).as_euler("xyz", degrees=True)
+            for v in orientations
         )
 
 

tests/test_distributors.py
@@ -138,11 +138,11 @@ class TestVolumetricRotations(unittest.TestCase):
         region_ids = np.asarray(
             voxel_set.data[:, 0][voxel_set.index_of(positions)], dtype=int
         )
-        rotations = self.netw.get_placement_set("a").load_additional("rotations")
+        rotations = np.array(self.netw.get_placement_set("a").load_rotations())
         # Regions without orientation field -> no rotation
         self.assertTrue(
             np.array_equal(
-                np.all(rotations == 0, axis=1), np.isin(region_ids, (100, 728, 744))
+                np.all(rotations == 0.0, axis=1), np.isin(region_ids, (100, 728, 744))
             )
         )
         # Regions with orientation field -> AIBS Flocculus and Lingula
@@ -154,4 +154,4 @@ class TestVolumetricRotations(unittest.TestCase):
         )
         self.assertTrue(np.all(-180.0 < rotations[pos_w_rot < 180.0]))
         # orientation field x component should be close to 0.
-        self.assertTrue(np.all(np.absolute(rotations[pos_w_rot][:, 0]) < 10))
+        self.assertTrue(np.all(np.absolute(rotations[pos_w_rot][:, 0]) < 0.5))