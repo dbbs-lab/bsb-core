01e35ed06719916dfb21470e001b975931e608ac
Robin De Schepper <robin.deschepper93@gmail.com>
2023-02-22 09:43:23 +0100

    added `get_placement_set` to sim cell

--- bsb/simulation/cell.py
+++ bsb/simulation/cell.py
@@ -12,13 +12,18 @@ class CellModel(SimulationComponent):
 
     cell_type = config.ref(refs.cell_type_ref, key="name")
     """
-    Whether this cell type is a relay type. Relay types, during simulation, instantly
-    transmit incoming spikes to their targets.
+    The cell type that this model represents
     """
     parameters = config.list(type=Parameter)
+    """
+    The parameters of the model.
+    """
 
     def __lt__(self, other):
         try:
             return self.name < other.name
         except Exception:
             return True
+
+    def get_placement_set(self):
+        return self.cell_type.get_placement_set()
