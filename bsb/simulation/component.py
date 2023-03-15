01834060bdde40dda93d267ea2b4b5a08b1e09f1
Robin De Schepper <robin.deschepper93@gmail.com>
2023-03-15 13:54:14 +0100

    component cleanup

--- bsb/simulation/component.py
+++ bsb/simulation/component.py
@@ -1,24 +1,15 @@
 from .. import config
-from .._util import SortableByAfter
+from .._util import obj_str_insert
 
 
 @config.node
-class SimulationComponent(SortableByAfter):
+class SimulationComponent:
     name = config.attr(key=True)
 
     @property
     def simulation(self):
         return self._config_parent._config_parent
 
-    @classmethod
-    def get_ordered(cls, objects):
-        return objects.values()  # No sorting of simulation components required.
-
-    def get_after(self):
-        return None if not self.has_after() else self.after
-
-    def create_after(self):
-        self.after = []
-
-    def has_after(self):
-        return hasattr(self, "after")
+    @obj_str_insert
+    def __str__(self):
+        return f"'{self.name}'"
