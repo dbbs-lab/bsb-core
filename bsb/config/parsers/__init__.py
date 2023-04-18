e826a52b60ff55a16e33507e00056bc30f4bbce6
Robin De Schepper <robin.deschepper93@gmail.com>
2023-04-18 14:23:43 +0200

    Automatic documentation generation for configuration nodes (#709)
    * wip, recommit later
    
    * Morphology pipeline can store end result
    
    * give underlying cast error
    
    * Fixed error with `has_own_init` checking incorrect class
    
    * Requirement handlers can check if the node data is written in shortform
    
    * Preferably parse swc with inhouse swc parser
    
    * Added NameSelector shorthand
    
    * Fixed unserializable `FileDependency` attributes
    
    * Fixed pipeline calls
    
    * Added morphology creation from swc data
    
    * Added stable morpho comparison and allowed indicesless MorphologySet
    
    * Removed useless print commands
    
    * added `hint` kwarg to pass hint example values to autoconfig
    
    * expose more type information on attr
    
    * don't attempt to invert `None`
    
    * renamed and exposed `get_config_attributes`
    
    * use more robust `getattr` instead of dict lookup in module refs
    
    * added parser lookup methods
    
    * added syntax specification and `generate` method to parser iface
    
    * return already callable objects directly from `function_` type handler
    
    * pass (unenforced) type information to the property arg
    
    * autoconfig all the nodes in the reference
    
    * bumped checkout action
    
    * Fixed inversion of method shortcut
    
    * catch method's attribute error and reraise as typeerror
    
    ---------
    
    Co-authored-by: Francesco Sheiban <frances.j.shei@gmail.com>

--- bsb/config/parsers/__init__.py
+++ bsb/config/parsers/__init__.py
@@ -1,2 +1,12 @@
 from ._parser import Parser
 from .json import JsonParser
+
+
+def get_parser_classes():
+    from ...plugins import discover
+
+    return discover("config.parsers")
+
+
+def get_parser(parser):
+    return get_parser_classes()[parser]()
