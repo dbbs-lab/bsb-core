b8269df431a30559170b0626e40d1302f142854c
Robin De Schepper <robin.deschepper93@gmail.com>
2023-05-17 16:54:09 +0200

    use placement simdata as source of PS

--- bsb/simulators/nest/cell.py
+++ bsb/simulators/nest/cell.py
@@ -8,19 +8,19 @@ class NestCell(CellModel):
     neuron_model = config.attr(type=str, default="iaf_psc_alpha")
     constants = config.dict(type=types.any_())
 
-    def create_population(self):
+    def create_population(self, simdata):
         import nest
 
-        n = len(self.get_placement_set())
+        n = len(simdata.placement[self])
         population = nest.Create(self.neuron_model, n) if n else nest.NodeCollection([])
         self.set_constants(population)
-        self.set_parameters(population)
+        self.set_parameters(population, simdata)
         return population
 
     def set_constants(self, population):
         population.set(self.constants)
 
-    def set_parameters(self, population):
-        ps = self.get_placement_set()
+    def set_parameters(self, population, simdata):
+        ps = simdata.placement[self]
         for param in self.parameters:
             population.set(param.name, param.get_value(ps))
