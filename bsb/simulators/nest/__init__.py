8693a59d41bd5f035cdfe706b9ae7306d214f05d
Robin De Schepper <robin.deschepper93@gmail.com>
2023-05-22 15:07:29 +0200

    eager load the nest devices

--- bsb/simulators/nest/__init__.py
+++ bsb/simulators/nest/__init__.py
@@ -1,6 +1,7 @@
 from bsb.simulation import SimulationBackendPlugin
 from .adapter import NestAdapter
 from .simulation import NestSimulation
+from .devices import *
 
 
 __plugin__ = SimulationBackendPlugin(Simulation=NestSimulation, Adapter=NestAdapter)
