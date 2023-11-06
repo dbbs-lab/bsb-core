bf54460f5e9a28f1a24258bcf60e3ee1a72e55ec
Robin De Schepper <robin.deschepper93@gmail.com>
2023-11-06 15:53:23 +0100

    move neuron to bsb-neuron

--- bsb/simulators/neuron/device.py
+++
@@ -1,8 +0,0 @@
-from bsb import config
-from bsb.simulation.device import DeviceModel
-from bsb.simulation.targetting import Targetting
-
-
-@config.dynamic(attr_name="device", auto_classmap=True)
-class NeuronDevice(DeviceModel):
-    targetting = config.attr(type=Targetting, required=True)
