470a2fc4e9fd68faa43c1dc3ca9b7cf38bf5169c
Robin De Schepper <robin.deschepper93@gmail.com>
2023-11-30 15:56:58 +0100

    fix circular import

--- bsb/placement/__init__.py
+++ bsb/placement/__init__.py
@@ -1,5 +1,7 @@
+# The strategy module needs to be imported before any module that uses the `NotParallel` mixin.
+from .strategy import PlacementStrategy  # isort: skip
 from .arrays import ParallelArrayPlacement
 from .import_ import CsvImportPlacement, ImportPlacement
 from .particle import ParticlePlacement, RandomPlacement
 from .satellite import Satellite
-from .strategy import Entities, FixedPositions, PlacementStrategy
+from .strategy import Entities, FixedPositions
