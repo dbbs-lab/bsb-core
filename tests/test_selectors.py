e355fef542c41a076204ae25d34800129e41a010
Robin De Schepper <robin.deschepper93@gmail.com>
2024-03-05 14:17:19 +0100

    cleaned up some more polluting test scaffolds

--- tests/test_selectors.py
+++ tests/test_selectors.py
@@ -1,13 +1,6 @@
-import json
-import os
-import random
-import string
-import sys
 import unittest
 
-import h5py
-import numpy as np
-from bsb_test import skip_nointernet, skip_parallel
+from bsb_test import RandomStorageFixture, skip_nointernet, skip_parallel
 
 from bsb.cell_types import CellType
 from bsb.config import Configuration
@@ -30,7 +23,7 @@ def spoof_combo(name, *names):
     ]
 
 
-class TestSelectors(unittest.TestCase):
+class TestSelectors(RandomStorageFixture, unittest.TestCase, engine_name="hdf5"):
     def assertPicked(self, picks, sel, loaders, msg=None):
         self.assertEqual(picks, [l.name for l in loaders if sel.pick(l)], msg=msg)
 
@@ -70,10 +63,8 @@ class TestSelectors(unittest.TestCase):
     @skip_parallel
     def test_cell_type_shorthand(self):
         ct = CellType(spatial=dict(morphologies=[{"names": "*"}]))
-        cfg = Configuration.default(
-            storage={"root": "selectors_test.hdf5"}, cell_types={"ct": ct}
-        )
-        s = Scaffold(cfg)
+        cfg = Configuration.default(cell_types={"ct": ct})
+        s = Scaffold(cfg, self.storage)
         s.morphologies.save("A", Morphology([Branch([[0, 0, 0]], [1])]), overwrite=True)
         self.assertEqual(1, len(ct.get_morphologies()), "Should select saved morpho")
         ct.spatial.morphologies[0].names = ["B"]
@@ -94,7 +85,7 @@ class TestSelectors(unittest.TestCase):
             )
         )
         cfg = Configuration.default(cell_types={"ct": ct})
-        s = Scaffold(cfg)
+        s = Scaffold(cfg, self.storage)
         self.assertIn(name, s.morphologies, "missing NM")
         m = s.morphologies.select(*ct.spatial.morphologies)[0]
         self.assertEqual(name, m.get_meta()["neuron_name"], "meta not stored")
@@ -112,7 +103,7 @@ class TestSelectors(unittest.TestCase):
             )
         )
         cfg = Configuration.default(cell_types={"ct": ct})
-        s = Scaffold(cfg)
+        s = Scaffold(cfg, self.storage)
         with self.assertRaises(SelectorError, msg="doesnt exist, should error"):
             err = None
             try:
