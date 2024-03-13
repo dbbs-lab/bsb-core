efc828bc8d6181dca32c43b9fe217d5552af9f53
Robin De Schepper <robin.deschepper93@gmail.com>
2024-03-13 17:59:42 +0100

    tty! :tada:

--- bsb/services/_pool_listeners.py
+++ bsb/services/_pool_listeners.py
@@ -6,7 +6,7 @@ from collections import defaultdict
 from typing import cast
 
 from blessed import Terminal
-from dashing import HSplit, Log, Text
+from dashing import Color, DoubleColumn, HSplit, Text
 
 from .pool import (
     Job,
@@ -34,24 +34,24 @@ class NonTTYTerminalListener(Listener):
 
 
 class TTYTerminalListener(Listener):
-    def __init__(self):
+    def __init__(self, fps: int):
         self._terminal = None
         self._last_update = 0
+        self.fps = fps
 
         self._ui = HSplit(
             Text(
-                " ",
+                "",
                 title="Progress",
+                color=Color.White,
+                border_color=Color.Yellow,
+            ),
+            DoubleColumn(
+                [],
+                title="Jobs",
                 color=7,
                 border_color=3,
             ),
-            Text(
-                " ",
-                title="Components",
-                color=7,
-                border_color=3,
-            ),
-            Log(title="Logs", color=7, border_color=3),
         )
 
     @contextlib.contextmanager
@@ -64,37 +64,49 @@ class TTYTerminalListener(Listener):
         self._context = self._terminal_context(self._terminal)
         self._ui._terminal = self._terminal
         self._context.__enter__()
+        self._start = time.perf_counter()
         self.update()
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self._context.__exit__(exc_type, exc_val, exc_tb)
 
     def __call__(self, progress: PoolProgress):
-        if (
-            progress.reason == PoolProgressReason.POOL_STATUS_CHANGE
-            and progress.status == PoolStatus.EXECUTING
-        ):
-            self._tally = PoolTally(JobTally)
-            for job in progress.jobs:
-                self._tally[job.name].tally(job)
-            cast(Text, self._ui.items[1]).text = "\n".join(
-                f"{k}: {v}" for k, v in self._tally.sorted()
-            )
+        self._ui.title = None
+        if progress.reason == PoolProgressReason.POOL_STATUS_CHANGE:
+            if progress.status == PoolStatus.SCHEDULING:
+                self._tally = PoolTally(JobTally)
+            if progress.status == PoolStatus.EXECUTING:
+                for job in progress.jobs:
+                    self._tally[job.name].tally(job)
+            self._pool_status = progress.status
+        self.update_progress()
         if (
             (progress.reason == PoolProgressReason.JOB_STATUS_CHANGE)
             and progress.status != JobStatus.PENDING
             and progress.status != JobStatus.QUEUED
         ):
             self._tally.update_tally(cast(PoolJobUpdateProgress, progress))
-            cast(Text, self._ui.items[1]).text = (
-                "\n".join(f"{k}: {v}" for k, v in self._tally.top()) or " "
-            )
+            self.update_jobs()
         self.update()
 
     def update(self):
-        if time.time() - self._last_update >= 1 / 25:
+        if time.time() - self._last_update >= 1 / self.fps:
             self._ui.display()
             self._last_update = time.time()
+
+    def update_jobs(self):
+        cast(DoubleColumn, self._ui.items[1]).lines = [
+            (name, str(tally)) for name, tally in self._tally.top()
+        ]
+
+    def update_progress(self):
+        cast(Text, self._ui.items[0]).text = (
+            f"Status: {str(self._pool_status).split('.')[1]}\n"
+            f"Job total:\n"
+            f" {self._tally}\n"
+            f"Elapsed: {round(time.perf_counter() - self._start, 2)}s\n"
+            f"Job est.: {round(self._tally.get_estimate(), 2)}s"
+        )
 
 
 class JobTally:
@@ -102,19 +114,22 @@ class JobTally:
         self._total = 0
         self._tallies = defaultdict(int)
 
+    @property
+    def total(self):
+        return self._total
+
     def tally(self, job: Job):
         self._tallies[job.status] += 1
         self._total += 1
 
     def tally_unfinished(self):
-        return (
-            self._tallies[JobStatus.PENDING]
-            + self._tallies[JobStatus.QUEUED]
-            + self._tallies[JobStatus.RUNNING]
-        )
+        return self.tally_status(JobStatus.PENDING, JobStatus.QUEUED, JobStatus.RUNNING)
 
     def tally_finished(self):
         return self._total - self.tally_unfinished()
+
+    def tally_status(self, *status: JobStatus):
+        return sum((self._tallies[s] for s in status), start=0)
 
     def progress(self):
         return self.tally_finished() / self._total
@@ -127,10 +142,22 @@ class JobTally:
         self._tallies[new_status] += 1
 
     def __str__(self):
-        return f"({self.tally_finished()}/{self._total})"
+        others = ""
+        if running := self.tally_status(JobStatus.RUNNING):
+            others += f"→ {running}/"
+        if failed := self.tally_status(JobStatus.FAILED, JobStatus.ABORTED):
+            others += f"🗙 {failed}/"
+        if skipped := self.tally_status(JobStatus.CANCELLED):
+            others += f"⊘ {skipped}/"
+        return f"({others}✔ {self.tally_status(JobStatus.SUCCESS)}/{self._total})"
 
 
 class PoolTally(defaultdict):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._elapsed = {}
+        self._durations = defaultdict(lambda: [0, 0])
+
     def sorted(self):
         yield from sorted(self.items(), key=lambda i: -i[1].progress())
 
@@ -139,3 +166,35 @@ class PoolTally(defaultdict):
 
     def update_tally(self, progress: PoolJobUpdateProgress):
         self[progress.job.name].update(progress.old_status, progress.status)
+        if progress.status == JobStatus.RUNNING:
+            self._elapsed[id(progress.job)] = time.perf_counter()
+        elif progress.old_status == JobStatus.RUNNING:
+            total, n = self._durations[progress.job.name]
+            self._durations[progress.job.name] = [
+                total + time.perf_counter() - self._elapsed[id(progress.job)],
+                n + 1,
+            ]
+            del self._elapsed[id(progress.job)]
+
+    def tally(self, *status: JobStatus):
+        if status:
+            return sum(tally.tally_status(*status) for tally in self.values())
+        else:
+            return sum(tally.total for tally in self.values())
+
+    def get_estimate(self):
+        return sum(
+            total / n * self[job_name].tally_unfinished()
+            for job_name, (total, n) in self._durations.items()
+            if n
+        )
+
+    def __str__(self):
+        others = ""
+        if running := self.tally(JobStatus.RUNNING):
+            others += f"→ {running}/"
+        if failed := self.tally(JobStatus.FAILED, JobStatus.ABORTED):
+            others += f"🗙 {failed}/"
+        if skipped := self.tally(JobStatus.CANCELLED):
+            others += f"⊘ {skipped}/"
+        return f"({others}✔ {self.tally(JobStatus.SUCCESS)}/{self.tally()})"
