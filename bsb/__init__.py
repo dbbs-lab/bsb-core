4166bb3c6dbbf4780b87a9d32efedaa742319ffc
Helveg <robin.deschepper93@gmail.com>
2024-03-28 18:32:44 +0100

    Bump version: 4.0.0-rc0 → 4.0.0-rc1

--- bsb/__init__.py
+++ bsb/__init__.py
@@ -7,7 +7,7 @@ which are essential for `bsb-core` to function. First time users are recommended
 install the `bsb` package instead.
 """
 
-__version__ = "4.0.0-rc0"
+__version__ = "4.0.0-rc1"
 
 import functools
 import importlib
