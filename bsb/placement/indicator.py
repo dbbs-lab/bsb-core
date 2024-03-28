8fa73cac93de57658742a97beb8b29042086da83
Dimitri RODARIE <d.rodarie@gmail.com>
2024-03-28 16:57:04 +0100

    Fix `count_ratio` estimation (#824)
    * Fix count_ratio estimate, add tests. See #822
    
    * Split tests on Placement Indicator between count and count_ratio.
    
    * Add feature relative to density_key.
    Add more tests.
    
    * Update bsb/placement/indicator.py
    
    ---------
    
    Co-authored-by: Robin De Schepper <robin.deschepper93@gmail.com>

--- bsb/placement/indicator.py
+++ bsb/placement/indicator.py
@@ -104,7 +104,6 @@ class PlacementIndicator:
                     )
                     * count_ratio
                 )
-                estimate = self._estim_for_chunk(chunk, estimate)
             elif density_ratio is not None:
                 # Create an indicator based on this strategy for the related CT.
                 # This means we'll read only the CT indications, and ignore any
@@ -113,16 +112,20 @@ class PlacementIndicator:
                 rel_ind = PlacementIndicator(self._strat, relation)
                 rel_density = rel_ind.indication("density")
                 rel_pl_density = rel_ind.indication("planar_density")
+                rel_pl_density_key = rel_ind.indication("density_key")
                 if rel_density is not None:
                     estimate = self._density_to_estim(rel_density * density_ratio, chunk)
                 elif rel_pl_density is not None:
                     estimate = self._pdensity_to_estim(
                         rel_pl_density * density_ratio, chunk
                     )
+                elif rel_pl_density_key is not None:
+                    # Use the relation's `guess` to guess according to the relation's density key
+                    estimate = rel_ind.guess(chunk, voxels) * density_ratio
                 else:
                     raise PlacementRelationError(
                         f"{self.cell_type.name} requires relation {relation.name}"
-                        + "to specify density information."
+                        + " to specify density information."
                     )
             else:
                 raise PlacementError(
