46120500babbe651904130b07bffbad732e0c6e6
drodarie <drodarie@users.noreply.github.com>
2024-10-12 16:20:07 +0000

    docs: bump version: v4.5.0 → v4.5.1 [skip ci]

--- bsb/__init__.py
+++ bsb/__init__.py
@@ -7,7 +7,7 @@ which are essential for `bsb-core` to function. First time users are recommended
 install the `bsb` package instead.
 """
 
-__version__ = "4.5.0"
+__version__ = "4.5.1"
 
 import functools
 import importlib
