c969586f9a6db0733339a5e128afc1aab3816007
drodarie <d.rodarie@gmail.com>
2024-12-12 07:39:52 +0100

    fix: postprocessing tests

--- tests/test_postprocessing.py
+++ tests/test_postprocessing.py
@@ -36,18 +36,19 @@ class TestAfterConnectivityHook(
 
     def test_after_connectivity_job(self):
         self.network.compile()
-        count_files = 0
-        for filename in os.listdir():
-            if filename.startswith(f"test_after_conn_{MPI.get_rank()}"):
-                count_files += 1
-                with open(filename, "r") as f:
-                    lines = f.readlines()
-                    self.assertEqual(
-                        len(lines), 1, "The postprocess should be called only once."
-                    )
-                    self.assertEqual(lines[0], "Test config\n")
-                os.remove(filename)
-        self.assertEqual(count_files, 1)
+        if MPI.get_rank() == 0:
+            count_files = 0
+            for filename in os.listdir():
+                if filename.startswith("test_after_conn_"):
+                    count_files += 1
+                    with open(filename, "r") as f:
+                        lines = f.readlines()
+                        self.assertEqual(
+                            len(lines), 1, "The postprocess should be called only once."
+                        )
+                        self.assertEqual(lines[0], "Test config\n")
+                    os.remove(filename)
+            self.assertEqual(count_files, 1)
 
 
 class TestAfterPlacementHook(RandomStorageFixture, unittest.TestCase, engine_name="hdf5"):
@@ -71,15 +72,16 @@ class TestAfterPlacementHook(RandomStorageFixture, unittest.TestCase, engine_nam
 
     def test_after_placement_job(self):
         self.network.compile()
-        count_files = 0
-        for filename in os.listdir():
-            if filename.startswith(f"test_after_place_{MPI.get_rank()}"):
-                count_files += 1
-                with open(filename, "r") as f:
-                    lines = f.readlines()
-                    self.assertEqual(
-                        len(lines), 1, "The postprocess should be called only once."
-                    )
-                    self.assertEqual(lines[0], "Test config\n")
-                os.remove(filename)
-        self.assertEqual(count_files, 1)
+        if MPI.get_rank() == 0:
+            count_files = 0
+            for filename in os.listdir():
+                if filename.startswith("test_after_place_"):
+                    count_files += 1
+                    with open(filename, "r") as f:
+                        lines = f.readlines()
+                        self.assertEqual(
+                            len(lines), 1, "The postprocess should be called only once."
+                        )
+                        self.assertEqual(lines[0], "Test config\n")
+                    os.remove(filename)
+            self.assertEqual(count_files, 1)
